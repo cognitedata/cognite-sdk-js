c04420192db096f0182058f3b852a3710867f297
f1cognite <fredrik.anfinsen@cognite.com>
2019-05-12 15:09:32 +0200

    Fixed Files tests

--- src/resources/files.ts
+++ src/resources/files.ts
@@ -120,8 +120,7 @@ function generateUploadEndpoint(
         data: fileMetadata,
       }
     );
-    // @ts-ignore (files API still wraps response with 'data')
-    const file = response.data.data;
+    const file = response.data;
     if (fileContent != null) {
       const { uploadUrl } = file;
       const headers: any = {};
