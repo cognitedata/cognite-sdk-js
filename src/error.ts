80d1c670fef14162d1add7e851cfbdaaae7d0043
f1cognite <fredrik.anfinsen@cognite.com>
2019-05-25 21:50:09 +0200

    fix: Support new error schema from API

--- src/error.ts
+++ src/error.ts
@@ -5,14 +5,36 @@ import { AxiosError } from 'axios';
 export class CogniteError extends Error {
   public status: number;
   public requestId?: string;
-  constructor(errorMessage: string, status: number, requestId?: string) {
+  public missing?: object[];
+  public duplicated?: any[];
+  constructor(
+    errorMessage: string,
+    status: number,
+    requestId?: string,
+    extra: any = {}
+  ) {
     let message = `${errorMessage} | code: ${status}`;
     if (requestId) {
       message += ` | X-Request-ID: ${requestId}`;
     }
+    const { missing, duplicated } = extra;
+    if (missing && missing.length > 0) {
+      message += '\nMissing:';
+      missing.forEach((item: object) => {
+        message += `\n  ${JSON.stringify(item)}`;
+      });
+    }
+    if (duplicated && duplicated.length > 0) {
+      message += '\nDuplicated:';
+      duplicated.forEach((item: object) => {
+        message += `\n  ${JSON.stringify(item)}`;
+      });
+    }
     super(message);
     this.status = status;
     this.requestId = requestId;
+    this.missing = missing;
+    this.duplicated = duplicated;
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, this.constructor);
     } else {
@@ -26,12 +48,16 @@ export function handleErrorResponse(err: AxiosError) {
   let code;
   let message;
   let requestId;
+  let missing;
+  let duplicated;
   try {
     code = err.response!.status;
     message = err.response!.data.error.message;
+    missing = err.response!.data.error.missing;
+    duplicated = err.response!.data.error.duplicated;
     requestId = (err.response!.headers || {})['X-Request-Id'];
   } catch (_) {
     throw err;
   }
-  throw new CogniteError(message, code, requestId);
+  throw new CogniteError(message, code, requestId, { missing, duplicated });
 }
