bc45861567d45eeb1cd1911d37f0197f39c1ce8a
polomani <ivan.polomanyi@cognite.com>
2019-06-02 14:23:16 +0200

    feat: alphabetizeTypes script

---
+++ scripts/alphabetizeTypes.js
@@ -0,0 +1,33 @@
+const Comments = require('parse-comments');
+const TypescriptParser = require('typescript-parser').TypescriptParser
+const argv = require('yargs').argv
+const readFileSync = require('fs').readFileSync;
+const writeFileSync = require('fs').writeFileSync;
+const comments = new Comments();
+const parser = new TypescriptParser();
+
+const FILENAME = argv.i;
+const HEADER = argv.h;
+
+(async () => {
+    const original = readFileSync(FILENAME).toString();
+    const parsedCode = await parser.parseFile(FILENAME).catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+    let result = HEADER + '\n';
+    const parsedComments = comments.parse(original);
+    const mapCodeToComment = {}
+    for (comment of parsedComments) {
+        mapCodeToComment[comment.codeStart] = comment.raw
+    }
+    parsedCode.declarations.sort((a, b) => (a.name < b.name) ? -1 : 1);
+    for (declaration of parsedCode.declarations) {
+        const { start, end } = declaration
+        if (mapCodeToComment[start]) {
+            result += '\n/*' + mapCodeToComment[start] + '*/';
+        }
+        result += '\n' + original.substring(start, end) + '\n';
+    }
+    writeFileSync(FILENAME, result)
+})();
