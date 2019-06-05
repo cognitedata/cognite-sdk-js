a72da89aec7aab536c0c4747417883f47873eb4f
polomani <ivan.polomanyi@cognite.com>
2019-06-05 20:41:04 +0200

    fix: remove extra types from custom

--- src/types/custom.ts
+++ src/types/custom.ts
@@ -1,11 +1,13 @@
 // Copyright 2019 Cognite AS
 import {
   AssetIdEither,
+  AzureADConfigurationDTO,
   BoundingBox3D,
   Cursor,
   ExternalAsset,
   FileFilter,
   FilesMetadata,
+  OAuth2ConfigurationDTO,
 } from './types';
 
 export interface FileRequestFilter extends Cursor, FileFilter {}
@@ -234,54 +236,6 @@ export interface InputProjectAuthentication {
 }
 
 /**
- * Data specific to Azure AD authentication
- */
-export interface AzureADConfigurationDTO {
-  /**
-   * Azure application ID. You get this when creating the Azure app.
-   */
-  appId?: string;
-  /**
-   * Azure application secret. You get this when creating the Azure app.
-   */
-  appSecret?: string;
-  /**
-   * Azure tenant ID.
-   */
-  tenantId?: string;
-  /**
-   * Resource to grant access to. This is usually (always?) 00000002-0000-0000-c000-000000000000
-   */
-  appResourceId?: string;
-}
-
-/**
  * List of valid domains. If left empty, any user registered with the OAuth2 provider will get access.
  */
 export type ValidDomains = string[];
-
-/**
- * Data related to generic OAuth2 authentication. Not used for Azure AD
- */
-export interface OAuth2ConfigurationDTO {
-  /**
-   * Login URL of OAuth2 provider. E.g https://accounts.google.com/o/oauth2/v2/auth.
-   */
-  loginUrl?: string;
-  /**
-   * Logout URL of OAuth2 provider. E.g https://accounts.google.com/Logout.
-   */
-  logoutUrl?: string;
-  /**
-   * URL to get access token from OAuth2 provider. E.g https://www.googleapis.com/oauth2/v4/token.
-   */
-  tokenUrl?: string;
-  /**
-   * Client ID. You probably get this when registering your client with the OAuth2 provider.
-   */
-  clientId?: string;
-  /**
-   * Client secret. You probably get this when registering your client with the OAuth2 provider.
-   */
-  clientSecret?: string;
-}
