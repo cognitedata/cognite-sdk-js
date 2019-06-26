25a58a7ef26b3656e6842fa2dd7d518f3a89fb84
Eirik Dahlen <eirik.dahlen@cognite.com>
2019-06-26 12:23:17 +0200

    feat: add asset and assetList class

---
+++ src/resources/classes/assetList.ts
@@ -0,0 +1,17 @@
+// Copyright 2019 Cognite AS
+
+import { API } from '../api';
+import { Asset } from './asset';
+
+export class AssetList extends Array<Asset> {
+  private client: API;
+  constructor(client: API, ...args: any) {
+    super(...args);
+    this.client = client;
+  }
+
+  public delete = () => {
+    return this.client.assets.delete(this.map(asset => ({ id: asset.id })));
+  }; 
+
+}
