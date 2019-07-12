e41c6a00768ec3bfff17e14bb0c4cc1f25c67c04
polomani <ivan.polomanyi@cognite.com>
2019-07-12 16:12:39 +0200

    chore: auto update js code samples after package deploy

---
+++ scripts/afterRelease.sh
@@ -0,0 +1,24 @@
+brew install hub
+yarn typedoc --json docs.json
+node scripts/postProcessDocs.js
+
+packageVersion=$(node -e "const fs = require('fs');\
+const packageFile = fs.readFileSync('./package.json');\
+const packageJson = JSON.parse(packageFile.toString());\
+console.log(packageJson.version)")
+branchName="feat/updateSDKJsExamples$packageVersion"
+
+hub clone git@github.com:cognitedata/service-contracts.git
+cd service-contracts
+
+hub checkout -b "$branchName"
+cp ../jsSnippets.json ./versions/v1/js-sdk-examples.json
+hub add ./versions/v1/js-sdk-examples.json
+hub commit -m "feat: update js code samples for sdk version $packageVersion"
+git push origin "$branchName"
+hub pull-request --no-edit -a polomani,f1cognite -r martincognite
+
+cd ../
+rm -rf service-contracts
+rm jsSnippets.json
+rm docs.json
