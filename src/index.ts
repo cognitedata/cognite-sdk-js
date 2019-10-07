7cde3712d3b25f3c466524bec182891af23acaa2
polomani <ivan.polomanyi@cognite.com>
2019-10-07 12:42:45 +0200

    fix: export asset and timeseries helper classes

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,7 @@ export * from './types/types';
 export { CogniteError } from './error';
 export { CogniteMultiError } from './multiError';
 export { CogniteLoginError } from './loginError';
+export { Asset as AssetClass } from './resources/classes/asset';
+export { TimeSeries as TimeSeriesClass } from './resources/classes/timeSeries';
+export { AssetList } from './resources/classes/assetList';
+export { TimeSeriesList } from './resources/classes/timeSeriesList';
