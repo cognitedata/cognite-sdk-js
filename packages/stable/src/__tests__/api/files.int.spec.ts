997aa845217516a2bdf20ec1b569ba911a1b2e60
haved <krogstie.havard@gmail.com>
2020-06-22 15:55:54 +0200

    feat(monorepo): further towards monorepo
     - Move samples folder to become a package
     - Use lerna commands for yarn scripts
     - Use exact versions of packages when cross referencing
     - Use package names when cross referencing
     - Use tsconfig hack for better IDE experience
     - Allow linting

--- packages/stable/src/__tests__/api/files.int.spec.ts
+++ packages/stable/src/__tests__/api/files.int.spec.ts
@@ -143,7 +143,7 @@ describe('Files integration test', () => {
       mimeType: 'application/octet-stream',
     };
     const fileContentBinary = readFileSync(
-      'packages/stable/src/__tests__/test3dFile.fbx'
+      'src/__tests__/test3dFile.fbx'
     );
     let binaryFile: FilesMetadata;
 
