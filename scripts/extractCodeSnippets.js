333245d27d62d394febcc57a693a2d293bd02504
Fredrik Anfinsen <fredrik.anfinsen@gmail.com>
2020-06-23 15:48:23 +0200

    chore: fix bug in docs generator for code snippets

--- scripts/extractCodeSnippets.js
+++ scripts/extractCodeSnippets.js
@@ -1,49 +1,87 @@
 const path = require('path');
 const snippetsFolder = path.join(__dirname, '../codeSnippets');
-const jsonDoc = require(snippetsFolder + '/docs.json');
+const jsDoc = require(snippetsFolder + '/docs.json');
 const _ = require('lodash');
 const fs = require('fs');
 const tsconfig = require('../tsconfig.json');
 
-const docRegEx = /https:\/\/(doc.cognitedata.com|docs.cognite.com)\/api\/v1\/#operation\/([a-zA-Z0-9]+)/g;
-const header =
-`import { CogniteClient, SequenceValueType } from '@cognite/sdk';\n
-const client = new CogniteClient({ appId: '[APP NAME]' });
-client.loginWithApiKey({
-  project: '[PROJECT]',
-  apiKey: '[API_KEY]'
-});\n\n
-(async () => {\n`;
-const footer = '\n})();';
+function stripMarkdownCodeSnippet(rawCode) {
+  return rawCode
+    .replace('```js', '')
+    .replace('```', '')
+    .trim();
+}
 
-const operationsWithHeader = ['redirectUrl'];
-
-const resultJson = {
-  language: "JavaScript",
-  label: "JavaScript SDK",
-  operations: {}
-};
-_.cloneDeepWith(jsonDoc, (value, _, object) => {
-  let matches;
-  
-  while (matches = docRegEx.exec(value)) {
-    const operationId = matches[2];
-    const rawCode = object.text;
-    let code = rawCode.replace('```js', '').replace('```', '').trim();
-    let codeToTest = code;
-    if (operationsWithHeader.indexOf(operationId) < 0) {
-      codeToTest = header + code + footer;
+function findAllCodeSnippetsInJsDoc(jsDoc) {
+  const codeSnippets = new Map(); // string => string[]
+  _.cloneDeepWith(jsDoc, (value, _, object) => {
+    const docRegEx = /https:\/\/(doc.cognitedata.com|docs.cognite.com)\/api\/v1\/#operation\/([a-zA-Z0-9]+)/g;
+    let matches;
+    while ((matches = docRegEx.exec(value))) {
+      const operationId = matches[2];
+      const rawCode = object.text;
+      if (!codeSnippets.has(operationId)) {
+        codeSnippets.set(operationId, []);
+      }
+      codeSnippets.get(operationId).push(stripMarkdownCodeSnippet(rawCode));
     }
-    fs.writeFileSync(`${snippetsFolder}/${operationId}.ts`, codeToTest);
-    resultJson.operations[operationId] = code;
+  });
+  // comments appears two times in jsdoc...
+  for (let operationId of codeSnippets.keys()) {
+    codeSnippets.set(operationId, _.uniq(codeSnippets.get(operationId)));
   }
-});
+  return codeSnippets;
+}
 
-fs.writeFileSync(snippetsFolder + '/index.json', JSON.stringify(resultJson, null, 2) + '\n');
+function joinSnippets(snippets) {
+  return snippets.join('\n\n');
+}
+
+function writeCodeSnippetFile(codeSnippets, filepath) {
+  const output = {
+    language: 'JavaScript',
+    label: 'JavaScript SDK',
+    operations: {},
+  };
+
+  codeSnippets.forEach((snippets, operationId) => {
+    output.operations[operationId] = joinSnippets(snippets);
+  });
+  fs.writeFileSync(filepath, JSON.stringify(output, null, 2) + '\n');
+}
+
+const codeSnippets = findAllCodeSnippetsInJsDoc(jsDoc);
+const operationsWithHeader = ['redirectUrl'];
+writeCodeSnippetFile(codeSnippets, snippetsFolder + '/index.json');
 console.log('JS code snippets saved to: jsSnippets.json');
 
+// write typescript file for syntax testing
+codeSnippets.forEach((snippets, operationId) => {
+  if (operationsWithHeader.indexOf(operationId) !== -1) {
+    return;
+  }
+  const codeToTest = `
+    import { CogniteClient, SequenceValueType } from '@cognite/sdk';
+    const client = new CogniteClient({ appId: '[APP NAME]' });
+    client.loginWithApiKey({
+      project: '[PROJECT]',
+      apiKey: '[API_KEY]'
+    });
+    (async () => {
+      ${joinSnippets(snippets)}
+    })();
+  `;
+  fs.writeFileSync(`${snippetsFolder}/${operationId}.ts`, codeToTest);
+});
+
+// write tsconfig file
 tsconfig.include = ['*.ts'];
 tsconfig.compilerOptions.noUnusedLocals = false;
 
-fs.writeFileSync(snippetsFolder + '/tsconfig.json', JSON.stringify(tsconfig, null, 2) + '\n');
-console.log(`TS config for code snippets saved to: ${snippetsFolder}/tsconfig.json`);
+fs.writeFileSync(
+  snippetsFolder + '/tsconfig.json',
+  JSON.stringify(tsconfig, null, 2) + '\n'
+);
+console.log(
+  `TS config for code snippets saved to: ${snippetsFolder}/tsconfig.json`
+);
