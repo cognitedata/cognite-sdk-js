7aff20ede126e83c7252709e1b1db43557da393f
haved <krogstie.havard@gmail.com>
2020-06-24 15:23:58 +0200

    ci: hack - use allow node util as external in rollup

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -14,6 +14,7 @@ export default {
     external: [
         ...Object.keys(pkg.dependencies || {}),
         ...Object.keys(pkg.peerDependencies || {}),
+        "util"
     ],
     plugins: [
         typescript({
