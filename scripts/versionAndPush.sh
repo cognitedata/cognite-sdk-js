d2606bf2df6324cc8b86b3a97d30c91a98781677
haved <krogstie.havard@gmail.com>
2020-06-27 09:44:04 +0200

    ci: fix git set origin

--- scripts/versionAndPush.sh
+++ scripts/versionAndPush.sh
@@ -18,7 +18,7 @@ if true ; then
     # Find the new remote with github token, careful not to leak secrets
     OLD_REMOTE="$(git remote get-url origin)"
     REPO_PATH=$(echo $OLD_REMOTE | sed -En "s/^.*github.com:(.*)/\1/p")
-    git remote set-url "https://$GITHUB_TOKEN@github.com/$REPO_PATH" --quiet > /dev/null 2>&1
+    git remote set-url origin "https://$GITHUB_TOKEN@github.com/$REPO_PATH" --quiet > /dev/null 2>&1
     echo "Set git origin with token!"
 
     GH_TOKEN="$GITHUB_TOKEN" #Used by lerna version
