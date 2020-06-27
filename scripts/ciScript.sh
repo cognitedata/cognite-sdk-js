5ddd92f343e834b14cc7e28f4a2343488d068247
haved <krogstie.havard@gmail.com>
2020-06-27 12:27:17 +0200

    ci: build docs before pushing

--- scripts/ciScript.sh
+++ scripts/ciScript.sh
@@ -5,18 +5,17 @@ if [ "$ONLY_TEST" = true ]; then
     echo "Only doing build and test"
     yarn --ignore-engines
     yarn global add lerna@3.1.4 #Supports node 8
-    lerna -v #print version
     lerna run build --stream
     lerna run test --stream
 else
     yarn
-    #yarn global add codecov
-    #yarn commitlint-travis
+    yarn global add codecov
+    yarn commitlint-travis
     #yarn validateDocLinks
-    #yarn lint
-    #yarn build
-    #yarn test && codecov
-    #yarn test-samples
-    #yarn test-snippets
-    #yarn bundle-docs
+    yarn lint
+    yarn build
+    yarn test && codecov
+    yarn test-samples
+    yarn test-snippets
+    yarn bundle-docs
 fi
