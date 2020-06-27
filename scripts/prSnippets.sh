504e461732b29f0d000d5c122d319b6375c6ee4e
haved <krogstie.havard@gmail.com>
2020-06-27 13:39:43 +0200

    ci: give more feedback, but don't leak info

--- scripts/prSnippets.sh
+++ scripts/prSnippets.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-# exit when any command fails
-set -ex
+# exit when any command fails (DON'T -x !!)
+set -e
 
 sudo add-apt-repository -y ppa:cpick/hub
 sudo apt-get -y update
@@ -12,10 +12,14 @@ packageVersion=$(jq -r ".version" "${stablePackage}/package.json")
 branchName="bot/jsCodeSnippets_v$packageVersion"
 message="[JS SDK]: update code snippets to v$packageVersion"
 
+echo "Cloning service contract repo"
 git clone https://$GITHUB_TOKEN@github.com/cognitedata/service-contracts.git >/dev/null 2>&1
 cd service-contracts
 
+echo "Checking out branch $branchName"
 git checkout -b "$branchName"
+
+echo "Copying stable SDK's code snippets"
 cp "../${stablePackage}/codeSnippets/index.json" ./versions/v1/js-sdk-examples.json
 if ! git diff --quiet ; then
     echo "service contracts code snippets have changed. making pull request"
@@ -23,7 +27,7 @@ if ! git diff --quiet ; then
     git commit -m "$message"
     #git push origin "$branchName"
     #hub pull-request -m "$message"
-    echo "pull request made"
+    #echo "pull request made"
 else
     echo "no changes made to code snippets, not opening service contracts pr"
 fi
