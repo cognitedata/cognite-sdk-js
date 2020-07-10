b80c7f32524905c7f6f44190d64e213cc58d44de
haved <krogstie.havard@gmail.com>
2020-07-10 12:09:40 +0200

    ci: freeze lockfile in ci and add check if packages have changed

--- scripts/ciScript.sh
+++ scripts/ciScript.sh
@@ -4,12 +4,12 @@ set -ex
 
 if [ "$ONLY_TEST" = true ]; then
     echo "Only doing build and test"
-    yarn --ignore-engines
+    yarn --frozen-lockfile --ignore-engines
     yarn global add lerna@3.1.4 #Supports node 8
     lerna run build --stream
     lerna run test --stream
 else
-    yarn
+    yarn --frozen-lockfile
     yarn global add codecov@3.7.0
     yarn commitlint-travis
     yarn validateDocLinks || true
