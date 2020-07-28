b5a15e0ae68cffc0d32ab3a211b6b31f4c242478
HKrogstie <krogstie.havard@gmail.com>
2020-07-28 13:24:40 +0200

    docs: improve description of committing in contributing (#404)
    * docs: improve description of commit rules
    
    * ci: add custom parser for commit message hook allowing bang
    
    * docs: add the bang option to docs
    
    * chore: let rules dictate empty subjects in commitlint
    
    * docs: fix typo and start list of commit types
    
    * docs: add example of subject line and all commit types
    
    * docs: use scope in example of commit message
    
    Co-authored-by: Ivan Polomani <ivan.polomanyi@cognite.com>
    
    * docs(readme): change example and add test type
    
    * docs(contributing-readme): use contributing-readme as scope
    
    Co-authored-by: Ivan Polomani <ivan.polomanyi@cognite.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,13 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w*)(\((.*)\))?!?: (.*)$/,
+      headerCorrespondence: ['type', '', 'scope', 'subject'],
+    },
+  },
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+  },
+};
