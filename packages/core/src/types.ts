c0e19d17d4046da9ea07f0a3f3e35f2e8fa6ab2c
HKrogstie <krogstie.havard@gmail.com>
2020-08-03 13:31:00 +0200

    docs: write docstring about CursorAndAsyncIterator (#406)
    * docs(reference): write docstring about CursorAndAsyncIterator
    
    * docs(reference): specify js in markdown for docstring
    
    * docs(types): explain CursorAndAsyncIterator more
    
    * docs(types): add more use of CursorAndAsyncAwait

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -58,6 +58,36 @@ export interface LogoutUrlResponse {
   data: { url: LogoutUrl };
 }
 
+/**
+ * A Promise to a response that can be awaited like normal,
+ * but at the risk of not getting all results due to API limits.
+ * In which case a nextCursor field is returned to request the next page.
+ * Helper methods are provided to abstract away the pagination.
+ *
+ * Example using `client.timeseries.list` with a per-request limit of 1000:
+ * ```js
+ * const response = client.timeseries.list({ filter: { assetIds: [ASSET_ID] }, limit: 1000 });
+ * ```
+ *
+ * You can get up to 1000 elements with normal await:
+ * ```js
+ * const { items, nextCursor } = await response;
+ * ```
+ *
+ * You can use `autoPagingToArray` to get more items than the per-request limit.
+ * E.g. an array of up to 5000 items:
+ * ```js
+ * const timeseries = await response.autoPagingToArray({ limit: 5000 });
+ * ```
+ * You may also specify `{ limit: Infinity }` to get all results.
+ *
+ * You can also iterate through all items (unless you break out of the loop) like so:
+ * ```js
+ * for await (const value of response) {
+ *   // do something to value
+ * }
+ * ```
+ */
 export type CursorAndAsyncIterator<T> = Promise<ListResponse<T[]>> &
   CogniteAsyncIterator<T>;
 
