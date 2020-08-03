1287e56440c7a82dffd4fc98df72701133880330
haved <krogstie.havard@gmail.com>
2020-08-03 15:36:35 +0200

    ci: remove major hardcoding in lerna version

--- scripts/versionAndPush.sh
+++ scripts/versionAndPush.sh
@@ -6,12 +6,6 @@ set -e
 echo "Checking out \$TRAVIS_BRANCH: $TRAVIS_BRANCH"
 git checkout $TRAVIS_BRANCH
 
-# Check if anything has changed in any package
-if ! yarn lerna changed; then
-    echo "Nothing has changed"
-    exit 0
-fi
-
 git config --global user.email "cognite-cicd@users.noreply.github.com"
 git config --global user.name "Cognite CICD"
 
@@ -19,19 +13,20 @@ git config --global user.name "Cognite CICD"
 git remote set-url origin "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > /dev/null 2>&1
 echo "Set git origin with token!"
 
-GH_TOKEN="$GITHUB_TOKEN" #Used by lerna version
+# Used by lerna version
+GH_TOKEN="${GITHUB_TOKEN}"
 # Lerna makes a commit (with [skip ci] in message)
 # Will fail if upstream head has changed in the meantime
+# Will do nothing and return 0 if nothing has changed
 echo "Running lerna version"
-yarn lerna version major --conventional-commits --create-release github --no-private --yes
+yarn lerna version --conventional-commits --create-release github --no-private --yes
 echo "Ran lerna version!"
 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 echo "Added NPM_TOKEN to .npmrc"
 
-# publish to npm
+# publish to npm (if any packages have new versions)
 echo "Running lerna publish"
 # Don't remove builds and snippet info etc after publish
-# We don't have prepublish lifecycle scripts, but the packages are already built either way
 yarn lerna publish from-package --yes --no-git-reset
 echo "Ran lerna publish!"
