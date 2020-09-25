8888e219866c76f19c49a593cbe86074256b1bb0
Roman Vizniak <mr.traser@gmail.com>
2020-09-25 09:29:18 +0300

    chore: add PR commitlint check

--- scripts/ciScript.sh
+++ scripts/ciScript.sh
@@ -5,6 +5,11 @@ set -ex
 yarn --frozen-lockfile
 yarn build
 
+if [ "$TRAVIS_EVENT_TYPE" = pull_reqest ]; then
+  PR_TITLE=$(curl https://github.com/${TRAVIS_REPO_SLUG}/pull/${TRAVIS_PULL_REQUEST} 2> /dev/null | grep "title" | head -1)
+  echo $PR_TITLE | commitlint
+fi
+
 if [ "$ONLY_TEST" = true ]; then
     echo "Only doing build and test"
     yarn test
