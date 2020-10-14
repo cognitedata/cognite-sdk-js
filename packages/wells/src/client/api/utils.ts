03a2ec2ac33e8048c0da58c35958791fa1e990c8
sollimann <kristoffer.solberg@cognite.com>
2020-10-14 16:46:08 +0200

    chore: remove unused enums

--- packages/wells/src/client/api/utils.ts
+++ packages/wells/src/client/api/utils.ts
@@ -6,14 +6,3 @@ export const geospatialClient = CogniteGeospatialClient({
   api_key: process.env.COGNITE_WELLS_CREDENTIALS as string,
   api_url: Constants.BASE_URL,
 });
-
-export enum SpatialRelationshipNameDTO {
-  Within = 'within',
-  WithinDistance = 'withinDistance',
-  WithinCompletely = 'withinCompletely',
-  Intersect = 'intersect',
-  Within3d = 'within3d',
-  WithinDistance3d = 'withinDistance3d',
-  WithinCompletely3d = 'withinCompletely3d',
-  Intersect3d = 'intersect3d',
-}
