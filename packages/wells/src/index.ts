e53a2e8496f673fa0e4087a145e80c2af51bb764
sollimann <kristoffer.solberg@cognite.com>
2020-10-19 14:02:36 +0200

    fix: export Wells and model in index.ts

--- packages/wells/src/index.ts
+++ packages/wells/src/index.ts
@@ -2,3 +2,9 @@
 
 export * from '@cognite/sdk';
 export { default as CogniteClient } from './client/cogniteClient';
+export { Wells } from './client/api/wells';
+
+// dataclasses and types
+export * from './client/model/Well';
+export * from './client/model/GeoJson';
+export * from './client/model/WellHeadLocation';
