6a1b7f24f367522c0024122c488ab352a8be0502
sollimann <kristoffer.solberg@cognite.com>
2020-10-30 14:09:56 +0100

    chore: added all integration tests for wellbores

--- packages/wells/src/__tests__/integration/wellbores.int.spec.ts
+++ packages/wells/src/__tests__/integration/wellbores.int.spec.ts
@@ -3,11 +3,8 @@
 import { setupLoggedInClient } from '../testUtils';
 import CogniteClient from '../../client/cogniteClient';
 import { SearchWellbores } from 'wells/src/client/model/Wellbore';
-//import { SearchWellbores, SearchWellbore } from 'wells/src/client/model/Wellbore';
-//import { assert } from 'console';
 
 // suggested solution/hack for conditional tests: https://github.com/facebook/jest/issues/3652#issuecomment-385262455
-
 const describeIfCondition =
   process.env.COGNITE_WELLS_PROJECT && process.env.COGNITE_WELLS_CREDENTIALS
     ? describe
@@ -21,16 +18,62 @@ describeIfCondition(
       client = setupLoggedInClient();
     });
 
-    test('standard filter - get all wellbores', async () => {
+    test('standard filter - list all wellbores', async () => {
       const response = await client.wellbores.listAll();
       expect(response.length).toBe(5);
+      response.forEach(element => {
+        expect(element.name).toContain('Wellbore');
+      });
     });
 
-    test('custom filter - get all wellbores', async () => {
+    test('custom filter - list all wellbores', async () => {
       const fn: SearchWellbores = async () => await client.wellbores.listAll();
 
       const response = await client.wellbores.listAll(fn);
       expect(response.length).toBe(5);
+      response.forEach(element => {
+        expect(element.name).toContain('Wellbore');
+      });
+    });
+
+    test('standard filter - list immediate children wellbores', async () => {
+      const wellId = 2278618537691581;
+      const response = await client.wellbores.listChildren(wellId);
+      response.forEach(element => {
+        expect(element.parentId).toBe(wellId);
+      });
+    });
+
+    test('custom filter - list immediate children wellbores ', async () => {
+      const fn: SearchWellbores = async (args: number) =>
+        await client.wellbores.listChildren(args);
+
+      const wellId = 2278618537691581;
+      const response = await client.wellbores.listChildren(wellId, fn);
+      response.forEach(element => {
+        expect(element.parentId).toBe(wellId);
+      });
+    });
+
+    test('standard filter - list all children ', async () => {
+      const rootId = 4438800495523058;
+      const response = await client.wellbores.listByWellId(rootId);
+      expect(response.length).toBe(5);
+      response.forEach(element => {
+        if (element.metadata) expect(element.metadata['type']).toBe('Wellbore');
+      });
+    });
+
+    test('custom filter - list all children ', async () => {
+      const fn: SearchWellbores = async (args: number) =>
+        await client.wellbores.listByWellId(args);
+
+      const rootId = 4438800495523058;
+      const response = await client.wellbores.listByWellId(rootId, fn);
+      expect(response.length).toBe(5);
+      response.forEach(element => {
+        if (element.metadata) expect(element.metadata['type']).toBe('Wellbore');
+      });
     });
   }
 );
