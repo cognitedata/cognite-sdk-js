0f384afd235b593e98d10653e9553bcac5c04d45
sollimann <kristoffer.solberg@cognite.com>
2020-11-02 23:47:16 +0100

    chore: added surveys

---
+++ packages/wells/src/client/api/surveys.ts
@@ -0,0 +1,42 @@
+import { Sequence, SequencesAPI } from '@cognite/sdk';
+import { SequenceRow } from 'stable/dist/src/api/sequences/sequenceRow';
+import { Survey } from '../model/Survey';
+
+export class Surveys extends SequencesAPI {
+  /**
+   *
+   * @param sequences
+   */
+  private mapToSurvey = (sequences: Sequence[]): Survey[] => {
+    return sequences.map(sequence => {
+      return <Survey>{
+        id: sequence.id,
+        name: sequence.name,
+        description: sequence.description,
+        assetId: sequence.assetId,
+        externalId: sequence.externalId,
+        metadata: sequence.metadata,
+        rows: async (): Promise<SequenceRow[]> => {
+          return this.retrieveRows({
+            externalId:
+              sequence.externalId != undefined ? sequence.externalId : '',
+          }).autoPagingToArray({ limit: 100 });
+        },
+      };
+    });
+  };
+
+  /**
+   *
+   * @param assetId
+   */
+  listTrajectories = async (assetId: number): Promise<Survey[]> => {
+    const sequences = await this.search({
+      filter: {
+        assetIds: [assetId],
+        metadata: { type: 'DefinitiveSurvey', PHASE: 'ACTUAL' },
+      },
+    });
+    return this.mapToSurvey(sequences);
+  };
+}
---
+++ packages/wells/src/client/model/Survey.ts
@@ -0,0 +1,39 @@
+import { SequenceRow } from 'stable/dist/src/api/sequences/sequenceRow';
+
+export interface Survey {
+  /**
+   * @type {number}
+   * @memberof Survey
+   */
+  id: number;
+  /**
+   * @type {string}
+   * @memberof Survey
+   */
+  name: string;
+  /**
+   * @type {string}
+   * @memberof Survey
+   */
+  description: string;
+  /**
+   * @type {number}
+   * @memberof Survey
+   */
+  assetId: number;
+  /**
+   * @type {string}
+   * @memberof Survey
+   */
+  externalId?: string;
+  /**
+   * Custom, application specific metadata. String key -> String value.
+   * @type {{ [key: string]: string; }}
+   * @memberof Well
+   */
+  metadata?: { [key: string]: string };
+  /**
+   *
+   */
+  rows(): Promise<SequenceRow[]>;
+}
