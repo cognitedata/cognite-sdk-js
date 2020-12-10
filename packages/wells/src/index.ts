d8b35ba86153c9376722062b513c1d7a5b1d91a3
Kristoffer Rakstad Solberg <kristoffer.solberg@cognite.com>
2020-12-10 19:11:54 +0100

    feat: remove deprecated files and update index (#460)
    * chore: update exported classes in index.ts
    
    * chore: remove deprecated files
    
    * chore: update index

--- packages/wells/src/index.ts
+++ packages/wells/src/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2020 Cognite AS
 
 export * from '@cognite/sdk';
-export { default as CogniteClient } from './client/cogniteClient-deprecated';
+export { default as CogniteWellsClient } from './client/CogniteWellsClient';
 export { WellsAPI } from './client/api/wellsApi';
 export { WellboresAPI } from './client/api/wellboresAPI';
 
 // dataclasses and types
-export * from './client/model/Well-deprecated';
 export * from './client/model/GeoJson';
-export * from './client/model/WellHeadLocation-deprecated';
+export * from './client/model/Well';
 export * from './client/model/Wellbore';
+export * from './client/model/WellHead';
+export * from './client/model/WellFilter';
+export * from './client/model/WellType';
 export * from './client/model/Survey';
 export * from './client/model/MeasurementType';
 export * from './client/model/Measurement';
-export * from './client/model/Well';
-export * from './client/model/WellFilter';
-export * from './client/model/WellHead';
+export * from './constants';
