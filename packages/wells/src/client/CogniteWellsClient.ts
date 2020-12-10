00a3f5e782370178262ff800cb15b3401acc6f25
sollimann <kristoffer.solberg@cognite.com>
2020-12-10 18:03:47 +0100

    chore: added surveysApi class and getData method

--- packages/wells/src/client/CogniteWellsClient.ts
+++ packages/wells/src/client/CogniteWellsClient.ts
@@ -1,9 +1,10 @@
 import { BaseCogniteClient } from '@cognite/sdk-core';
 import { accessApi } from '@cognite/sdk-core';
 import { WellsAPI } from './api/wellsApi';
+import { WellboresAPI } from './api/wellboresApi';
+import { SurveysAPI } from './api/surveysApi';
 import { version } from '../../package.json';
 import { WELL_SERVICE_BASE_URL } from './api/utils';
-import { WellboresAPI } from './api/wellboresAPI';
 
 export default class CogniteWellsClient extends BaseCogniteClient {
   public get wells() {
@@ -14,21 +15,31 @@ export default class CogniteWellsClient extends BaseCogniteClient {
     return accessApi(this.wellboresApi);
   }
 
+  public get surveys() {
+    return accessApi(this.surveysApi);
+  }
+
   private wellsApi?: WellsAPI;
   private wellboresApi?: WellboresAPI;
+  private surveysApi?: SurveysAPI;
 
   protected initAPIs() {
-    // wells
     this.setBaseUrl(WELL_SERVICE_BASE_URL);
+
+    // wells
     this.wellsApi = this.apiFactory(WellsAPI, 'wells');
     this.wellsApi.setHttpClient = this.httpClient;
     this.wellsApi.setProject = this.project;
 
     // wellbores
-    this.setBaseUrl(WELL_SERVICE_BASE_URL);
     this.wellboresApi = this.apiFactory(WellboresAPI, 'wellbores');
     this.wellboresApi.setHttpClient = this.httpClient;
     this.wellboresApi.setProject = this.project;
+
+    // wellbores
+    this.surveysApi = this.apiFactory(SurveysAPI, 'surveys');
+    this.surveysApi.setHttpClient = this.httpClient;
+    this.surveysApi.setProject = this.project;
   }
 
   protected get version() {
--- packages/wells/src/client/model/Survey.ts
+++ packages/wells/src/client/model/Survey.ts
@@ -30,3 +30,101 @@ export interface Survey {
    */
   rows(limit?: number): Promise<SequenceRow[]>;
 }
+
+export const enum ValueType {
+  string = 'STRING',
+  double = 'DOUBLE',
+  long = 'LONG',
+}
+
+export interface SurveyDataRequest {
+  /**
+   * @type {number}
+   * @memberof SurveyDataRequest
+   */
+  id: number;
+  /**
+   * @type {number}
+   * @memberof SurveyDataRequest
+   */
+  start?: number;
+  /**
+   * @type {number}
+   * @memberof SurveyDataRequest
+   */
+  end?: number;
+  /**
+   * @type {number}
+   * @memberof SurveyDataRequest
+   */
+  limit?: number;
+  /**
+   * @type {number}
+   * @memberof SurveyDataRequest
+   */
+  cursor?: string;
+  /**
+   * @type {string[]}
+   * @memberof SurveyDataRequest
+   */
+  columns?: string[];
+}
+
+export interface SurveyColumnInfo {
+  /**
+   * @type {string}
+   * @memberof SurveyDataRequest
+   */
+  externalId?: string;
+  /**
+   * @type {string}
+   * @memberof SurveyDataRequest
+   */
+  name?: string;
+  /**
+   * @type {ValueType}
+   * @memberof SurveyDataRequest
+   */
+  valueType?: ValueType;
+}
+
+export interface SurveyRow {
+  /**
+   * @type {number}
+   * @memberof SurveyDataRequest
+   */
+  rowNumber: number;
+  /**
+   * @type {any[]}
+   * @memberof SurveyDataRequest
+   */
+  values: any[];
+}
+
+export interface SurveyData {
+  /**
+   * @type {number}
+   * @memberof SurveyData
+   */
+  id: number;
+  /**
+   * @type {string[]}
+   * @memberof SurveyData
+   */
+  columns: string[];
+  /**
+   * @type {SurveyRow[]}
+   * @memberof SurveyData
+   */
+  rows: SurveyRow[];
+  /**
+   * @type {string}
+   * @memberof SurveyData
+   */
+  externalId?: string;
+  /**
+   * @type {number}
+   * @memberof SurveyData
+   */
+  nextCursor?: string;
+}
