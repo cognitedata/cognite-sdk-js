c83fef631acc14522059396156ab025a75837976
sollimann <kristoffer.solberg@cognite.com>
2020-12-14 15:36:53 +0100

    chore: update index

--- packages/wells/src/index.ts
+++ packages/wells/src/index.ts
@@ -1,10 +1,7 @@
 // Copyright 2020 Cognite AS
-
 export * from '@cognite/sdk';
 export { default as CogniteWellsClient } from './client/CogniteWellsClient';
 export { createWellsClient } from './client/api/utils';
-//export { WellsAPI } from './client/api/wellsApi';
-//export { WellboresAPI } from './client/api/wellboresApi';
 
 // dataclasses and types
 export * from './client/model/Cluster';
@@ -14,13 +11,7 @@ export * from './client/model/Wellbore';
 export * from './client/model/WellHead';
 export * from './client/model/WellFilter';
 export * from './client/model/WellType';
-export {
-  Survey,
-  SurveyDataRequest,
-  SurveyData,
-  SurveyRow,
-  SurveyColumnInfo,
-} from './client/model/Survey';
+export * from './client/model/Survey';
 export { MeasurementType } from './client/model/MeasurementType';
 export { Measurement, Measurements } from './client/model/Measurement';
 export * from './constants';
