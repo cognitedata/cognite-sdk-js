6929ac96db78a2882ca48b1762155508952f8940
sollimann <kristoffer.solberg@cognite.com>
2021-01-07 11:58:06 +0100

    chore: drafting of new client

---
+++ packages/wells/src/client/BaseWellsClient.ts
@@ -0,0 +1,410 @@
+// Copyright 2020 Cognite AS
+
+// import isFunction from 'lodash/isFunction';
+import isObject from 'lodash/isObject';
+import isString from 'lodash/isString';
+//import { LoginAPI } from 'core/src/api/login/loginApi';
+//import { LogoutApi } from 'core/src/api/logout/logoutApi';
+import {
+  X_CDF_APP_HEADER,
+  X_CDF_SDK_HEADER,
+  API_KEY_HEADER,
+} from 'core/src/constants';
+import { HttpHeaders, HttpRequestOptions, HttpResponse } from '..';
+import {
+  CDFHttpClient,
+  createUniversalRetryValidator,
+  MetadataMap,
+  RetryValidator,
+} from '@cognite/sdk-core';
+import { OnAuthenticate, OnTokens } from 'core/src/login';
+import { getBaseUrl, isUsingSSL, projectUrl } from 'core/src/utils';
+import { version } from 'os';
+
+export interface ClientOptions {
+  /** App identifier (ex: 'FileExtractor') C*/
+  appId: string;
+  baseUrl?: string;
+}
+
+export interface Project {
+  /**
+   * Cognite project to login into
+   */
+  project: string;
+}
+
+export interface ApiKeyLoginOptions extends Project {
+  /**
+   * A Cognite issued api-key
+   */
+  apiKey: string;
+}
+
+export const REDIRECT = 'REDIRECT';
+export const POPUP = 'POPUP';
+export interface OAuthLoginOptions extends Project {
+  onAuthenticate?: OnAuthenticate | 'REDIRECT' | 'POPUP';
+  onTokens?: OnTokens;
+  /**
+   * Provide optional cached access token to skip the authentication flow (client.authenticate will still override this).
+   */
+  accessToken?: string;
+}
+
+export function accessApi<T>(api: T | undefined): T {
+  if (api === undefined) {
+    throw Error(
+      'Need to login with either loginWithApiKey or loginWithOAuth before you can use the Cognite SDK'
+    );
+  }
+  return api;
+}
+
+export function throwReLogginError() {
+  throw Error(
+    'You cannot re-login with an already logged in Cognite client instance. Try to create a new Cognite client instance instead.'
+  );
+}
+
+export default class BaseWellsClient {
+  // public get login() {
+  //   return this.loginApi;
+  // }
+  // public get logout() {
+  //   return this.logoutApi;
+  // }
+
+  public http: CDFHttpClient;
+  public metadata: MetadataMap;
+  public projectName: string = '';
+  public hasBeenLoggedIn: boolean = false;
+  //public loginApi: LoginAPI;
+  //public logoutApi: LogoutApi;
+  /**
+   * Create a new SDK client
+   *
+   * @param options Client options
+   *
+   * ```js
+   * import { CogniteClient } from '@cognite/sdk';
+   *
+   * const client = new CogniteClient({ appId: 'YOUR APPLICATION NAME' });
+   *
+   * // can also specify a base URL
+   * const client = new CogniteClient({ ..., baseUrl: 'https://greenfield.cognitedata.com' });
+   * ```
+   */
+  constructor(options: ClientOptions) {
+    if (!isObject(options)) {
+      throw Error('`CogniteClient` is missing parameter `options`');
+    }
+    if (!isString(options.appId)) {
+      throw Error('options.appId is required and must be of type string');
+    }
+    const { baseUrl } = options;
+    this.http = new CDFHttpClient(
+      getBaseUrl(baseUrl),
+      this.getRetryValidator()
+    );
+    this.httpClient
+      .setDefaultHeader(X_CDF_APP_HEADER, options.appId)
+      .setDefaultHeader(
+        X_CDF_SDK_HEADER,
+        `CogniteJavaScriptSDK:${this.version}`
+      );
+
+    this.metadata = new MetadataMap();
+    //this.loginApi = new LoginAPI(this.httpClient, this.metadataMap);
+    //this.logoutApi = new LogoutApi(this.httpClient, this.metadataMap);
+  }
+
+  public authenticate: () => Promise<boolean> = async () => {
+    throw Error(
+      'You can only call authenticate after you have called loginWithOAuth'
+    );
+  };
+
+  public get project() {
+    return this.projectName;
+  }
+
+  /**
+   * Login client with api-key
+   *
+   * @param options Login options
+   *
+   * ```js
+   * import { CogniteClient } from '@cognite/sdk';
+   *
+   * const client = new CogniteClient({ appId: '[YOUR APP NAME]' });
+   * client.loginWithApiKey({
+   *   apiKey: '[API KEY]',
+   *   project: '[PROJECT]',
+   * });
+   * // after login you can do calls with the client
+   * const createdAsset = await client.assets.create([{ name: 'My first asset' }]);
+   * ```
+   */
+  public loginWithApiKey = (options: ApiKeyLoginOptions) => {
+    if (this.hasBeenLoggedIn) {
+      throwReLogginError();
+    }
+
+    if (!isObject(options)) {
+      throw Error('`loginWithApiKey` is missing parameter `options`');
+    }
+    const { project, apiKey } = options;
+    const keys: (keyof ApiKeyLoginOptions)[] = ['project', 'apiKey'];
+    for (const property of keys) {
+      if (!isString(options[property])) {
+        throw Error(
+          `options.${property} is required and must be of type string`
+        );
+      }
+    }
+    this.projectName = project;
+    this.httpClient.setDefaultHeader(API_KEY_HEADER, apiKey);
+
+    this.initAPIs();
+    this.hasBeenLoggedIn = true;
+  };
+
+  /**
+   * Login client with OAuth login flow
+   * <!-- [Login with redirect](https://doc.cognitedata.com/api/v1/#operation/redirectUrl) -->
+   *
+   * ```js
+   * import { CogniteClient, REDIRECT } from '@cognite/sdk';
+   *
+   * const client = new CogniteClient({ appId: '[YOUR APP NAME]' });
+   *
+   * client.loginWithOAuth({
+   *   project: '[PROJECT]',
+   *   onAuthenticate: REDIRECT // optional, REDIRECT is by default
+   * });
+   * // after login you can do calls with the client
+   * (async () => {
+   *   const createdAsset = await client.assets.create([{ name: 'My first asset' }]);
+   * })();
+   * ```
+   *
+   * @param options Login options
+   */
+  public loginWithOAuth = (options: OAuthLoginOptions) => {
+    if (this.hasBeenLoggedIn) {
+      throwReLogginError();
+    }
+
+    if (!isObject(options)) {
+      throw Error('`loginWithOAuth` is missing parameter `options`');
+    }
+    const { project } = options;
+    if (!isString(project)) {
+      throw Error('options.project is required and must be of type string');
+    }
+    this.projectName = project;
+
+    if (!isUsingSSL()) {
+      console.warn(
+        'You should use SSL (https) when you login with OAuth since CDF only allows redirecting back to an HTTPS site'
+      );
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    // const onTokens = options.onTokens || (() => {});
+    // const onAuthenticate =
+    //   options.onAuthenticate === POPUP
+    //     ? onAuthenticateWithPopup
+    //     : isFunction(options.onAuthenticate)
+    //       ? options.onAuthenticate
+    //       : onAuthenticateWithRedirect;
+
+    // const authenticate = createAuthenticateFunction({
+    //   project,
+    //   httpClient: this.httpClient,
+    //   onAuthenticate,
+    //   onTokens,
+    // });
+
+    // this.httpClient.set401ResponseHandler(async (_, retry, reject) => {
+    //   const didAuthenticate = await authenticate();
+    //   return didAuthenticate ? retry() : reject();
+    // });
+
+    const { accessToken } = options;
+    if (accessToken) {
+      this.httpClient.setBearerToken(accessToken);
+    }
+
+    this.initAPIs();
+    // this.authenticate = authenticate;
+    this.hasBeenLoggedIn = true;
+  };
+
+  /**
+   * To modify the base-url at any point in time
+   */
+  public setBaseUrl = (baseUrl: string) => {
+    this.httpClient.setBaseUrl(baseUrl);
+  };
+
+  /**
+   * Returns the base-url used for all requests.
+   */
+  public getBaseUrl(): string {
+    return this.httpClient.getBaseUrl();
+  }
+
+  /**
+   * Returns the default HTTP request headers, including e.g. authentication
+   * headers that is included in all requests. Headers provided per-requests is not
+   * included in this list. This function is useful when constructing API requests
+   * outside the SDK.
+   *
+   * ```js
+   * const customUrl = '...';
+   * const headers = client.getDefaultRequestHeaders();
+   * const result = await fetch(customUrl, { headers });
+   * ```
+   */
+  public getDefaultRequestHeaders(): HttpHeaders {
+    return { ...this.httpClient.getDefaultHeaders() };
+  }
+
+  /**
+   * Lookup response metadata from an request using the response as the parameter
+   *
+   * ```js
+   * const createdAsset = await client.assets.create([{ name: 'My first asset' }]);
+   * const metadata = client.getMetadata(createdAsset);
+   * ```
+   */
+  public getMetadata = (value: any) => this.metadataMap.get(value);
+
+  /**
+   * Basic HTTP method for GET
+   *
+   * @param path The URL path
+   * @param options Request options, optional
+   *
+   * ```js
+   * const response = await client.get('/api/v1/projects/{project}/assets', { params: { limit: 50 }});
+   * ```
+   */
+  public get = <T = any>(path: string, options?: HttpRequestOptions) =>
+    this.httpClient.get<T>(path, options);
+
+  /**
+   * Basic HTTP method for PUT
+   *
+   * @param path The URL path
+   * @param options Request options, optional
+   *
+   * ```js
+   * const response = await client.put('someUrl');
+   * ```
+   */
+  public put = <T = any>(path: string, options?: HttpRequestOptions) =>
+    this.httpClient.put<T>(path, options);
+
+  /**
+   * Basic HTTP method for POST
+   *
+   * @param path The URL path
+   * @param options Request options, optional
+   *
+   * ```js
+   * const assets = [{ name: 'First asset' }, { name: 'Second asset' }];
+   * const response = await client.post('/api/v1/projects/{project}/assets', { data: { items: assets } });
+   * ```
+   */
+  public post = <T = any>(path: string, options?: HttpRequestOptions) =>
+    this.httpClient.post<T>(path, options);
+
+  /**
+   * Basic HTTP method for DELETE
+   *
+   * @param path The URL path
+   * @param options Request options, optional
+   * ```js
+   * const response = await client.delete('someUrl');
+   * ```
+   */
+  public delete = <T = any>(path: string, options?: HttpRequestOptions) =>
+    this.httpClient.delete<T>(path, options);
+
+  /**
+   * Basic HTTP method for PATCH
+   *
+   * @param path The URL path
+   * @param options Request options, optional
+   * ```js
+   * const response = await client.patch('someUrl');
+   * ```
+   */
+  public patch = <T = any>(path: string, options?: HttpRequestOptions) =>
+    this.httpClient.patch<T>(path, options);
+
+  public setOneTimeSdkHeader(value: string) {
+    this.httpClient.addOneTimeHeader(X_CDF_SDK_HEADER, value);
+  }
+
+  protected initAPIs() {
+    // will be overritten by subclasses
+  }
+
+  /**
+   * Returns the retry validator to be used in the http client.
+   * Override to provide a better validator
+   */
+  protected getRetryValidator(): RetryValidator {
+    return createUniversalRetryValidator();
+  }
+
+  protected get version() {
+    return `${version}-core`;
+  }
+
+  protected apiFactory = <ApiType>(
+    api: new (
+      relativePath: string,
+      httpClient: CDFHttpClient,
+      map: MetadataMap
+    ) => ApiType,
+    relativePath: string
+  ) => {
+    return new api(
+      `${this.projectUrl}/${relativePath}`,
+      this.httpClient,
+      this.metadataMap
+    );
+  };
+
+  protected get projectUrl() {
+    return projectUrl(this.project);
+  }
+
+  protected get metadataMap() {
+    return this.metadata;
+  }
+
+  protected get httpClient() {
+    return this.http;
+  }
+}
+
+// function onAuthenticateWithRedirect(login: OnAuthenticateLoginObject) {
+//   login.redirect({
+//     redirectUrl: window.location.href,
+//   });
+// }
+
+// function onAuthenticateWithPopup(login: OnAuthenticateLoginObject) {
+//   login.popup({
+//     redirectUrl: window.location.href,
+//   });
+// }
+
+export type BaseRequestOptions = HttpRequestOptions;
+export type Response = HttpResponse<any>;
