6ea47bac28ac6491ce2df9229979dfcb7fbb8c77
Roman Vizniak <mr.traser@gmail.com>
2021-03-18 11:57:09 +0200

    docs: change sample for cognite auth (popup flow)

--- samples/react/authentication-with-popup/src/App.js
+++ samples/react/authentication-with-popup/src/App.js
@@ -1,15 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CogniteClient, isLoginPopupWindow, loginPopupHandler, POPUP } from '@cognite/sdk';
 
 const project = 'publicdata';
+const client = new CogniteClient({ appId: 'Cognite SDK samples' });
+const renderAssetsInTable = (assets) => {
+  return (
+    <table>
+      <tbody>
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+      </tr>
+      {assets.map(asset => (
+        <tr key={asset.id}>
+          <td>{asset.name}</td>
+          <td>{asset.description}</td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  );
+}
 
-class App extends Component {
-  state = {
-    client: null,
-    assets: null,
-  };
+const App = () => {
+  const [assets, setAssets] = useState();
+  const [isInit, setIsInit] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
-  async componentDidMount() {
+  const fetchRootAssets = async () => {
+    try {
+      const assets = await client.assets
+        .list()
+        .autoPagingToArray({ limit: 10 });
+
+      setAssets(assets);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  const authenticate = async () => {
+    const result = await client.authenticate();
+
+    setIsSignedIn(result);
+  }
+
+  useEffect(() => {
     if (isLoginPopupWindow()) {
       loginPopupHandler();
       return;
@@ -19,57 +54,25 @@ class App extends Component {
     // to avoid the browser blocking the popup window. See https://stackoverflow.com/a/2587692/4462088
     // You can call `login.popup` whenever you want. The SDK will pause all requests until you have called it.
     // So it is possible to create a user-interface with a "Click here to login"-button that triggers `login.popup`.
-    const client = new CogniteClient({ appId: 'Cognite SDK samples' });
-    client.loginWithOAuth({
-      project,
-      onAuthenticate: POPUP,
-    });
+    const init = async () => {
+      const result = await client.loginWithOAuth({
+        project,
+        onAuthenticate: POPUP,
+      });
 
-    this.setState({ client });
-  }
-
-  fetchRootAssets = async () => {
-    const { client } = this.state;
-    if (client === null) return;
-    // fetch the first 10 (maximum) assets
-    try {
-      const assets = await client.assets
-        .list()
-        .autoPagingToArray({ limit: 10 });
-      this.setState({ assets });
-    } catch (err) {
-      console.error(err);
+      setIsInit(true);
+      setIsSignedIn(result);
     }
-  };
 
-  renderAssetsInTable = (assets) => {
-    return (
-      <table>
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-          </tr>
-          {assets.map(asset => (
-            <tr key={asset.id}>
-              <td>{asset.name}</td>
-              <td>{asset.description}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-
-  render() {
-    const { assets } = this.state;
-    return (
-      <div className="App">
-        <button onClick={this.fetchRootAssets}><h1>Click here to fetch assets from Cognite</h1></button>
-        {assets && this.renderAssetsInTable(assets)}
-      </div>
-    );
-  }
-}
+    init();
+  }, []);
+  return (
+    <div className="App">
+      <button disabled={!isInit || isSignedIn} onClick={authenticate}><h1>Authenticate</h1></button>
+      <button disabled={!isInit || !isSignedIn} onClick={fetchRootAssets}><h1>Click here to fetch assets from Cognite</h1></button>
+      {assets && renderAssetsInTable(assets)}
+    </div>
+  );
+};
 
 export default App;
