181a9c2dfd1a1a4b4b5be40b6d9a241b6dae5058
Emil Sandstø <emil.sandsto@gmail.com>
2021-06-10 23:52:44 +0200

    chore(release): lock version to a specific date (#513)

--- packages/beta/src/cogniteClient.ts
+++ packages/beta/src/cogniteClient.ts
@@ -82,6 +82,8 @@ export default class CogniteClient extends CogniteClientCleaned {
   protected initAPIs() {
     super.initAPIs();
 
+    // Lock version to the following date
+    this.httpClient.setDefaultHeader('cdf-version', 'V20210406');
     this.templateGroupsApi = this.apiFactory(
       TemplateGroupsApi,
       'templategroups'
