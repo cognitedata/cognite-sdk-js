ce3e063d9be3c115a5e376605b4ee63f69219816
Anders Øen Fylling <anders@nordic.email>
2021-06-23 14:51:09 +0200

    test: do a cdf call with the directoryPrefix filter

--- packages/stable/src/__tests__/api/files.int.spec.ts
+++ packages/stable/src/__tests__/api/files.int.spec.ts
@@ -76,6 +76,12 @@ describe.skip('Files integration test', () => {
     expect(file.geoLocation).toEqual(localFileMeta.geoLocation);
   });
 
+  test('validate directoryPrefix format against CDF', async () => {
+    await client.files.list({ filter: {
+      directoryPrefix: "/test" },
+    });
+  });
+
   test('retrieve', async () => {
     const [retrievedFile] = await client.files.retrieve([{ id: file.id }]);
     expect(retrievedFile.mimeType).toBe(localFileMeta.mimeType);
