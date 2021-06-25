a74d0b65dbe6d7808f5112ee1bce5d5e0fbecd7a
Anders Øen Fylling <anders@nordic.email>
2021-06-25 16:24:17 +0200

    test: directory prefix json format

---
+++ packages/stable/src/__tests__/api/files.unit.spec.ts
@@ -0,0 +1,44 @@
+// Copyright 2020 Cognite AS
+
+import nock from 'nock';
+import CogniteClient from '../../cogniteClient';
+import { setupMockableClient } from '../testUtils';
+import { mockBaseUrl } from '../testUtils';
+
+describe('Files unit test', () => {
+  let client: CogniteClient;
+  beforeEach(() => {
+    client = setupMockableClient();
+    nock.cleanAll();
+  });
+
+  test('filter with directoryPrefix', async () => {
+    nock(mockBaseUrl)
+      .post(new RegExp('/files/filter'), {
+        filter: {
+          directoryPrefix: '/test'
+        },
+      })
+      .once()
+      .reply(200, {
+        items: [
+          {
+            id: 928734,
+            name: "some_name",
+            uploaded: false,
+            createdTime: 93795847835,
+            lastUpdatedTime: 93795847835,
+            directory: '/test/functional'
+          }
+        ]
+      });
+
+    const resp = await client.files.list({
+      filter: {
+        directoryPrefix: '/test',
+      },
+    });
+    expect(resp[0].directory).toContain('/test')
+  });
+
+});
