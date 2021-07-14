11aca9498a8aef23c7855d675dc9d4d6cf70f10c
Anders Øen Fylling <7851860+andersfylling@users.noreply.github.com>
2021-07-14 14:04:07 +0200

    feat: add api version parameter to BaseCogniteClient [release] (#610)

--- packages/core/src/__tests__/constants.spec.ts
+++ packages/core/src/__tests__/constants.spec.ts
@@ -1,11 +1,7 @@
 // Copyright 2020 Cognite AS
 
-import { API_VERSION, BASE_URL } from '../constants';
+import { BASE_URL } from '../constants';
 
 test('BASE_URL', () => {
   expect(BASE_URL).toMatchInlineSnapshot(`"https://api.cognitedata.com"`);
-});
-
-test('API_VERSION', () => {
-  expect(API_VERSION).toMatchInlineSnapshot(`"v1"`);
 });
--- packages/core/src/baseCogniteClient.ts
+++ packages/core/src/baseCogniteClient.ts
@@ -27,6 +27,7 @@ import {
   isUsingSSL,
   projectUrl,
   isBrowser,
+  CogniteAPIVersion,
 } from './utils';
 import { version } from '../package.json';
 import {
@@ -122,6 +123,7 @@ export default class BaseCogniteClient {
     return this.logoutApi;
   }
 
+  private readonly apiVersion: CogniteAPIVersion;
   private readonly http: CDFHttpClient;
   private readonly metadata: MetadataMap;
   private readonly loginApi: LoginAPI;
@@ -145,7 +147,7 @@ export default class BaseCogniteClient {
    * const client = new CogniteClient({ ..., baseUrl: 'https://greenfield.cognitedata.com' });
    * ```
    */
-  constructor(options: ClientOptions) {
+  constructor(options: ClientOptions, apiVersion: CogniteAPIVersion = 'v1') {
     if (!isObject(options)) {
       throw Error('`CogniteClient` is missing parameter `options`');
     }
@@ -167,6 +169,7 @@ export default class BaseCogniteClient {
     this.metadata = new MetadataMap();
     this.loginApi = new LoginAPI(this.httpClient, this.metadataMap);
     this.logoutApi = new LogoutApi(this.httpClient, this.metadataMap);
+    this.apiVersion = apiVersion;
   }
 
   public authenticate: () => Promise<boolean> = async () => {
@@ -523,7 +526,7 @@ export default class BaseCogniteClient {
   };
 
   protected get projectUrl() {
-    return projectUrl(this.project);
+    return projectUrl(this.project, this.apiVersion);
   }
 
   protected get metadataMap() {
@@ -743,9 +746,12 @@ export default class BaseCogniteClient {
 
   protected async validateAccessToken(token: string): Promise<boolean> {
     try {
-      const response = await this.httpClient.get<any>('/api/v1/token/inspect', {
-        headers: { [AUTHORIZATION_HEADER]: bearerString(token) },
-      });
+      const response = await this.httpClient.get<any>(
+        `/api/${this.apiVersion}/token/inspect`,
+        {
+          headers: { [AUTHORIZATION_HEADER]: bearerString(token) },
+        }
+      );
 
       const { projects } = response.data;
 
