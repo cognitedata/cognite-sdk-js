2fd54b3a7980759c760d9e8307b0d08f3c7208b6
Eric Brattli <eric@brattli.net>
2021-08-19 16:20:33 +0200

    feat(functions): add functions for SDK (#646)
    * feat(functions): add functions for SDK
    
    * feat(functions): don't upload file every time
    
    * feat(functions): this instead of super
    
    * feat(functions): wrap arrow function in braces

---
+++ packages/playground/src/__tests__/api/functionsApi.int.spec.ts
@@ -0,0 +1,131 @@
+import CogniteClientPlayground from '../../cogniteClientPlayground';
+import { setupLoggedInClient } from '../testUtils';
+import { setupLoggedInClient as setupLoggedInCdfClient } from '@cognite/sdk/src/__tests__/testUtils';
+import { CogniteClient } from '@cognite/sdk';
+import { Function } from '../../types';
+import { matchers } from '../customMatchers';
+
+import fs from 'fs';
+import util from 'util';
+
+const readFile = util.promisify(fs.readFile);
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+describe('functions api', () => {
+  let client: CogniteClientPlayground;
+  let cdfClient: CogniteClient;
+  const functionName = 'test_function';
+  const testFunctionName = 'integration_test_function';
+  let testFunction: Function;
+
+  beforeAll(async () => {
+    expect.extend(matchers);
+    client = setupLoggedInClient();
+    cdfClient = setupLoggedInCdfClient();
+
+    testFunction = await client.functions
+      .list({ name: testFunctionName })
+      .then(res => res.items[0]);
+  });
+
+  test.skip('upload file', async () => {
+    await uploadFile(cdfClient, 'testFunction');
+  });
+
+  test('create -> list -> delete functions', async () => {
+    const fileId = await cdfClient.files
+      .list({
+        filter: { name: 'testFunction.zip' },
+      })
+      .then(res => res.items[0].id);
+    const createResponse = await client.functions.create([
+      {
+        name: functionName,
+        fileId,
+      },
+    ]);
+    const listResponse = await client.functions.list(
+      { name: functionName },
+      10
+    );
+    await client.functions.delete([{ id: listResponse.items[0].id }]);
+
+    expect(createResponse.length).toEqual(1);
+    expect(listResponse.items).toContainObject({
+      name: functionName,
+      fileId: createResponse[0].fileId,
+    });
+  });
+
+  test('retrieve functions', async () => {
+    const response = await client.functions.retrieve([{ id: testFunction.id }]);
+
+    expect(response.length).toEqual(1);
+    expect(response[0]).toStrictEqual(testFunction);
+  });
+
+  test('call function', async () => {
+    const callResponse = await client.functions.calls.callFunction(
+      testFunction.id,
+      { data: 'test data' }
+    );
+    let functionCall = await client.functions.calls
+      .retrieve(testFunction.id, [callResponse.id])
+      .then(calls => calls[0]);
+    const functionListCall = await client.functions.calls
+      .list(testFunction.id)
+      .then(res => res.items[0]);
+
+    while (functionCall.status !== 'Completed') {
+      await sleep(500);
+      functionCall = await client.functions.calls
+        .retrieve(testFunction.id, [callResponse.id])
+        .then(calls => calls[0]);
+    }
+
+    const logs = await client.functions.calls.retrieveLogs(
+      testFunction.id,
+      functionCall.id
+    );
+
+    const functionCallResponse = await client.functions.calls.retrieveResponse(
+      testFunction.id,
+      functionCall.id
+    );
+    expect(logs.items).toContainObject({ message: 'Hello World!' });
+    expect(functionCallResponse.functionId).toEqual(testFunction.id);
+    expect(functionCall.functionId).toEqual(testFunction.id);
+    expect(functionListCall.id).toEqual(functionCall.id);
+  });
+
+  test('function schedules create, list, delete', async () => {
+    const schedule = await client.functions.schedules
+      .create([
+        {
+          name: 'testSchedule',
+          cronExpression: '*/5 * * * *',
+          functionId: testFunction.id,
+        },
+      ])
+      .then(res => res[0]);
+    const schedules = await client.functions.schedules.list({
+      name: schedule.name,
+    });
+    await client.functions.schedules.delete([schedule.id]);
+
+    expect(schedules.items).toContainObject(schedule);
+  });
+});
+
+const uploadFile = async (client: CogniteClient, name: string) => {
+  const data = await readFile(
+    `packages/playground/src/__tests__/${name}/${name}.zip`
+  );
+  const file = await client.files.upload(
+    {
+      name: 'test_function.zip',
+    },
+    data.buffer
+  );
+  return file.id;
+};
---
+++ packages/playground/src/api/functions/functionCallsApi.ts
@@ -0,0 +1,74 @@
+import { CogniteInternalId } from '@cognite/sdk';
+import {
+  BaseResourceAPI,
+  CursorResponse,
+  ItemsResponse,
+} from '@cognite/sdk-core';
+import {
+  FunctionCall,
+  FunctionCallFilter,
+  FunctionId,
+  FunctionCallResponse,
+  FunctionCallLogEntry,
+} from '../../types';
+
+export class FunctionCallsApi extends BaseResourceAPI<FunctionCall> {
+  public retrieve = async (
+    functionId: CogniteInternalId,
+    ids: CogniteInternalId[]
+  ) => {
+    const path = this.url(`${functionId}/calls/byids`);
+    return this.callEndpointWithMergeAndTransform(
+      ids.map(id => ({ id })),
+      request => this.callRetrieveEndpoint(request, path)
+    );
+  };
+
+  public callFunction = async (
+    functionId: FunctionId,
+    data?: any,
+    nonce?: string
+  ) => {
+    const response = await this.post<FunctionCall>(
+      this.url(`${functionId}/call`),
+      {
+        data: { data, nonce },
+      }
+    );
+    return response.data;
+  };
+
+  public list = async (
+    functionId: CogniteInternalId,
+    filter?: FunctionCallFilter,
+    limit?: number,
+    cursor?: string
+  ) => {
+    const path = this.url(`${functionId}/calls/list`);
+    return this.listEndpoint(
+      async params =>
+        this.post<CursorResponse<FunctionCall[]>>(path, { data: params }),
+      { filter, limit, cursor }
+    );
+  };
+
+  public retrieveLogs = async (
+    functionId: CogniteInternalId,
+    callId: CogniteInternalId
+  ) => {
+    const response = await this.get<ItemsResponse<FunctionCallLogEntry>>(
+      this.url(`${functionId}/calls/${callId}/logs`)
+    );
+    return response.data;
+  };
+
+  public retrieveResponse = async (
+    functionId: CogniteInternalId,
+    callId: CogniteInternalId
+  ) => {
+    const response = await this.get<FunctionCallResponse>(
+      this.url(`${functionId}/calls/${callId}/response`)
+    );
+    return response.data;
+  };
+}
