d2aa36d764ba6b3c773d5185bd3526d47f92868d
ebrattli <eric.brattli@cognite.com>
2021-08-19 13:46:29 +0200

    feat(functions): add functions for SDK

---
+++ packages/playground/src/api/functions/functionSchedulesApi.ts
@@ -0,0 +1,40 @@
+// Copyright 2021 Cognite AS
+
+import { ItemsResponse } from '@cognite/sdk';
+import { BaseResourceAPI, CogniteInternalId } from '@cognite/sdk-core';
+import {
+  FunctionSchedule,
+  FunctionScheduleCreate,
+  FunctionSchedulesFilter,
+} from '../../types';
+
+export class FunctionSchedules extends BaseResourceAPI<FunctionSchedule> {
+  public retrieve = (ids: CogniteInternalId[], ignoreUnknownIds?: boolean) =>
+    super.retrieveEndpoint(ids.map(id => ({ id })), {
+      params: ignoreUnknownIds,
+    });
+
+  public list = async (filter?: FunctionSchedulesFilter, limit?: number) => {
+    const response = await this.post<ItemsResponse<Function>>(
+      this.listPostUrl,
+      {
+        data: { filter, limit },
+      }
+    );
+    return response.data;
+  };
+
+  public create = async (items: FunctionScheduleCreate[]) =>
+    super.createEndpoint(items);
+
+  public delete = async (ids: CogniteInternalId[]) =>
+    super.deleteEndpoint(ids.map(id => ({ id })));
+
+  public retrieveInputData = async (id: CogniteInternalId) => {
+    const response = await this.post<{ id: CogniteInternalId; data: object }>(
+      this.url(`${id}/input_data`)
+    );
+
+    return response.data;
+  };
+}
