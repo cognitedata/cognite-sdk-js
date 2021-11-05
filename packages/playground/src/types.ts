62ec20244345bd8ee2d3c8ce08c9580c9b7d595e
Anders Øen Fylling <anders@nordic.email>
2021-11-05 15:48:12 +0100

    chore: move classifiers to own file

--- packages/playground/src/types.ts
+++ packages/playground/src/types.ts
@@ -7,6 +7,7 @@ import {
 } from '@cognite/sdk';
 
 export * from './api/documents/shared';
+export * from './api/documents/classifierTypes';
 
 // This file is here mostly to allow apis to import { ... } from '../../types';
 // Overriding types should probably be done in their respective API endpoint files, where possible
