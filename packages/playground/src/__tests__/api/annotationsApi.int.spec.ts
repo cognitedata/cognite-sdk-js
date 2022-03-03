a6de4cce0912a2c39e577a28c783fb52fac47764
Peet Cremer <peet.cremer@cognite.com>
2022-03-03 18:19:02 +0100

    Revert "test: temporary skip the tests for the annotations endpoints (#770)"
    This reverts commit 055a5b0076cec74090c001065dece3ae69ba1873.

--- packages/playground/src/__tests__/api/annotationsApi.int.spec.ts
+++ packages/playground/src/__tests__/api/annotationsApi.int.spec.ts
@@ -48,9 +48,7 @@ const FILE_FILTER: AnnotationFilterProps = {
   annotatedResourceIds: [{ externalId: ANNOTATED_FILE_EXTERNAL_ID }],
 };
 
-// Annotations API tests are temporarily skipped due to the tests failing, which blocks non-related releases.
-// The Annotation endpoint has changed behavior, leading these tests to produce 400 errors.
-describe.skip('Annotations API', () => {
+describe('Annotations API', () => {
   let client: CogniteClientPlayground;
   let stableClient: CogniteClient;
   const createdAnnotationIds: InternalId[] = [];
