344125bfe106663e809415a82c8f583ab73e729a
anders fylling <anders@nordic.email>
2022-05-18 09:28:30 +0200

    style: fix

--- packages/stable/src/api/documents/documentsApi.ts
+++ packages/stable/src/api/documents/documentsApi.ts
@@ -75,9 +75,7 @@ export class DocumentsAPI extends BaseResourceAPI<Document> {
     return this.addToMapAndReturn(response.data, response);
   }
 
-  private async documentContent(
-    id: CogniteInternalId
-  ): Promise<string> {
+  private async documentContent(id: CogniteInternalId): Promise<string> {
     const response = await this.get<string>(this.url(`${id}/content`), {
       headers: {
         accept: 'text/plain',
