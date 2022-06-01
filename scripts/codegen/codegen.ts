d4fcb06d851abe38bd1554fbe5b87585849aa050
anders fylling <anders@nordic.email>
2022-06-01 10:52:51 +0200

    chore: add nodejs version guard

--- scripts/codegen/codegen.ts
+++ scripts/codegen/codegen.ts
@@ -7,6 +7,13 @@ const parser = yargsBase().options({
     package: { type: 'string', demand: true,  describe: "Js sdk package (stable, beta, etc.)" },
     service: { type: 'string', demand: false, describe: "REST service to generate types for" },
 }).command("generate", "generate types for configured services", (y) => y, async (argv) => {
+    const nodeVersion = process.versions.node.split(".");
+    const majorVersion = parseInt(nodeVersion[0]);
+    if (majorVersion < 16) {
+        console.error("NodeJS version must be v16 or higher");
+        process.exit(1);
+    }
+
     try {
         await cmd.generate({...argv});
         console.log("ok");
