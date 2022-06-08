8957b8d32f14f4176707542f91f926b871c66053
anders fylling <anders@nordic.email>
2022-06-09 00:38:11 +0200

    chore: remove dead args

--- scripts/codegen/snapshot.ts
+++ scripts/codegen/snapshot.ts
@@ -6,8 +6,6 @@ const cmd = new SnapshotCommand();
 const parser = yargsBase().options({
     package: { type: 'string', demand: true,  describe: "Js sdk package (stable, beta, etc.)" },
     service: { type: 'string', demand: false, describe: "REST service to generate types for" },
-    version: { type: 'string', demand: false, describe: "Cognite api snapshot (v1, playground, etc.)" },
-    "from-path": { type: 'string', demand: false, describe: "Specify a local openapi spec instead" },
 }).command("update", "update a snapshot", (y) => y, async (argv) => {
     await cmd.update({...argv});
 }).command("delete", "delete a snapshot", (y) => y, async (argv) => {
