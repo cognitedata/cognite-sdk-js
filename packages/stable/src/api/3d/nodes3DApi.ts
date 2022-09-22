80bc2c7b09eb2f3993b98b9c27df5939d7949a2a
cognite-ornellas <rodrigo.ornellas@cognite.com>
2022-09-22 10:44:33 -0300

    Revert "chore: improved documentation (#841)"
    This reverts commit 34904a129f58236efd9d12a058bd3c174e1bc84f.

--- packages/stable/src/api/3d/nodes3DApi.ts
+++ packages/stable/src/api/3d/nodes3DApi.ts
@@ -11,13 +11,6 @@ import {
 } from '../../types';
 
 export class Nodes3DAPI extends BaseResourceAPI<Node3D> {
-  /**
-   * [List 3D nodes](https://doc.cognitedata.com/api/v1/#operation/get3DNodes)
-   *
-   * ```js
-   * const nodes3d = await client.nodes3DApi.list(8252999965991682, 4190022127342195);
-   * ```
-   */
   public list(
     modelId: CogniteInternalId,
     revisionId: CogniteInternalId,
@@ -30,23 +23,6 @@ export class Nodes3DAPI extends BaseResourceAPI<Node3D> {
     );
   }
 
-  /**
-   * [Filter 3D nodes](https://docs.cognite.com/api/v1/#operation/filter3DNodes)
-   *
-   * ```js
-   * const query = {
-   *  filter: {
-   *    properties: {
-   *      Items: {
-   *        Type: ["Cylinder"]
-   *      }
-   *    }
-   *  },
-   *  partition: "1/10"
-   * };
-   * const nodes3d = await client.nodes3DApi.filter(8252999965991682, 4190022127342195, query);
-   * ```
-   */
   public filter(
     modelId: CogniteInternalId,
     revisionId: CogniteInternalId,
@@ -59,13 +35,6 @@ export class Nodes3DAPI extends BaseResourceAPI<Node3D> {
     );
   }
 
-  /**
-   * [Get 3D nodes by ID](https://docs.cognite.com/api/v1/#operation/get3DNodesById)
-   *
-   * ```js
-   * const nodes3d = await client.nodes3DApi.retrieve(8252999965991682, 4190022127342195, [{id: 123}, {id: 456}]);
-   * ```
-   */
   public retrieve = (
     modelId: CogniteInternalId,
     revisionId: CogniteInternalId,
@@ -75,13 +44,6 @@ export class Nodes3DAPI extends BaseResourceAPI<Node3D> {
     return super.retrieveEndpoint(ids, {}, path);
   };
 
-  /**
-   * [List 3D ancestor nodes](https://doc.cognitedata.com/api/v1/#operation/get3DNodeAncestors)
-   *
-   * ```js
-   * const nodes3d = await client.nodes3DApi.listAncestors(8252999965991682, 4190022127342195, 572413075141081);
-   * ```
-   */
   public listAncestors(
     modelId: CogniteInternalId,
     revisionId: CogniteInternalId,
