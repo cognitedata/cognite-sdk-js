e42e5664e1808f519ef1fef081c28accb9a9c78f
Baiel <baielrysbekov@gmail.com>
2023-01-17 14:30:02 +0100

    feat(stable): separate cogniteClient mock and int tests

--- packages/stable/src/__tests__/cogniteClient.int.spec.ts
+++ packages/stable/src/__tests__/cogniteClient.int.spec.ts
@@ -1,17 +1,12 @@
 // Copyright 2020 Cognite AS
 
-import nock from 'nock';
 import CogniteClient from '../cogniteClient';
 import { Asset, ItemsWrapper } from '../types';
-import {
-  mockBaseUrl,
-  setupLoggedInClient,
-  setupMockableClient,
-} from './testUtils';
+import { setupLoggedInClient } from './testUtils';
 
 describe('http methods - integration', () => {
   let client: CogniteClient;
-  const project = process.env.COGNITE_PROJECT as string;
+  const project = 'platypus'; //process.env.COGNITE_PROJECT as string;
   beforeAll(async () => {
     client = setupLoggedInClient();
   });
@@ -26,62 +21,5 @@ describe('http methods - integration', () => {
   test('get method', async () => {
     const response = await client.get(`/api/v1/projects/${project}/assets`);
     expect(response.data).toHaveProperty('items');
-  });
-});
-
-describe('api endpoints smoke test', () => {
-  let client: CogniteClient;
-
-  beforeEach(() => {
-    nock.cleanAll();
-    client = setupMockableClient();
-    const emptyResponse = { items: [] };
-    nock(mockBaseUrl).post(/.*/).times(Infinity).reply(200, emptyResponse);
-    nock(mockBaseUrl).get(/.*/).times(Infinity).reply(200, emptyResponse);
-    nock(mockBaseUrl).put(/.*/).once().reply(200, emptyResponse);
-  });
-
-  test('call (some) endpoints with a null context', async () => {
-    async function callApi(api: any) {
-      return Promise.all([
-        callEndpoint(api.create, []),
-        callEndpoint(api.list, {}),
-        callEndpoint(api.retrieve, []),
-        callEndpoint(api.update, []),
-        callEndpoint(api.search, []),
-        callEndpoint(api.delete, []),
-        callEndpoint(api.insert, []),
-      ]);
-    }
-
-    async function callEndpoint(endpoint: (a: any[]) => any, param: any) {
-      if (endpoint) {
-        const mockFn = jest.fn();
-        await endpoint.bind(null)(param).catch(mockFn);
-        expect(mockFn).not.toBeCalled();
-      }
-    }
-
-    await Promise.all(
-      [
-        client.assets,
-        client.apiKeys,
-        client.assets,
-        client.datapoints,
-        client.events,
-        client.files,
-        client.files3D,
-        client.groups,
-        client.login,
-        client.logout,
-        client.models3D,
-        client.projects,
-        client.raw,
-        client.securityCategories,
-        client.serviceAccounts,
-        client.timeseries,
-        client.viewer3D,
-      ].map(callApi)
-    );
   });
 });
---
+++ packages/stable/src/__tests__/cogniteClient.unit.spec.ts
@@ -0,0 +1,62 @@
+// Copyright 2020 Cognite AS
+
+import nock from 'nock';
+import CogniteClient from '../cogniteClient';
+import { mockBaseUrl, setupMockableClient } from './testUtils';
+
+describe('api endpoints smoke test', () => {
+  let client: CogniteClient;
+
+  beforeEach(() => {
+    nock.cleanAll();
+    client = setupMockableClient();
+    const emptyResponse = { items: [] };
+    nock(mockBaseUrl).post(/.*/).times(Infinity).reply(200, emptyResponse);
+    nock(mockBaseUrl).get(/.*/).times(Infinity).reply(200, emptyResponse);
+    nock(mockBaseUrl).put(/.*/).once().reply(200, emptyResponse);
+  });
+
+  test('call (some) endpoints with a null context', async () => {
+    async function callApi(api: any) {
+      return Promise.all([
+        callEndpoint(api.create, []),
+        callEndpoint(api.list, {}),
+        callEndpoint(api.retrieve, []),
+        callEndpoint(api.update, []),
+        callEndpoint(api.search, []),
+        callEndpoint(api.delete, []),
+        callEndpoint(api.insert, []),
+      ]);
+    }
+
+    async function callEndpoint(endpoint: (a: any[]) => any, param: any) {
+      if (endpoint) {
+        const mockFn = jest.fn();
+        await endpoint.bind(null)(param).catch(mockFn);
+        expect(mockFn).not.toBeCalled();
+      }
+    }
+
+    await Promise.all(
+      [
+        client.assets,
+        client.apiKeys,
+        client.assets,
+        client.datapoints,
+        client.events,
+        client.files,
+        client.files3D,
+        client.groups,
+        client.login,
+        client.logout,
+        client.models3D,
+        client.projects,
+        client.raw,
+        client.securityCategories,
+        client.serviceAccounts,
+        client.timeseries,
+        client.viewer3D,
+      ].map(callApi)
+    );
+  });
+});
