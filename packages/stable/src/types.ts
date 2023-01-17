de7c3443debb6cebfdcb1ee7b4b60186ac591016
Håkon Flatval <hakon.flatval@cognite.com>
2023-01-17 11:26:06 +0100

    feat: add translation and scale fields to 3D revision

--- packages/stable/src/types.ts
+++ packages/stable/src/types.ts
@@ -1990,6 +1990,16 @@ export interface Revision3D {
    * The rotation is expressed by Euler angles in radians and in XYZ order.
    */
   rotation?: Tuple3<number>;
+  /**
+   * Global translation to be applied to the entire model.
+   * The translation is expressed in meters.
+   */
+  translation?: Tuple3<number>;
+  /**
+   * Global scale to be applied to the entire model.
+   * The scale is expressed as scale along the X, Y and Z axes.
+   */
+  scale?: Tuple3<number>;
   camera?: RevisionCameraProperties;
   /**
    * The status of the revision.
@@ -2525,6 +2535,16 @@ export interface UpdateRevision3D {
      * The rotation is expressed by Euler angles in radians and in XYZ order.
      */
     rotation?: SetField<Tuple3<number>>;
+    /**
+     * Global translation to be applied to the entire model.
+     * The translation is expressed in meters.
+     */
+    translation?: SetField<Tuple3<number>>;
+    /**
+     * Global scale to be applied to the entire model.
+     * The scale is expressed as scale along the X, Y and Z axes.
+     */
+    scale?: SetField<Tuple3<number>>;
     /**
      * Initial camera target.
      */
