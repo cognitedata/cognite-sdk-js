d100485df14d7de273e778cef1946364192640f4
JiMin Ha <borajimin@gmail.com>
2023-02-09 13:00:02 +0100

    fix: try updating ts to 4.2.3

--- samples/nodejs/legacy-auth-typescript/quickstart.ts
+++ samples/nodejs/legacy-auth-typescript/quickstart.ts
@@ -19,17 +19,15 @@ async function quickstart() {
     appId: 'Cognite SDK samples',
     project,
     apiKeyMode: true,
-    getToken: () => Promise.resolve(apiKey)
+    getToken: () => Promise.resolve(apiKey),
   });
 
   console.log(
-    `\nThe SDK client is now configured to talk with Cognite project "${
-      project
-    }"\n`
+    `\nThe SDK client is now configured to talk with Cognite project "${project}"\n`
   );
 
   // get maximum 5 root assets
-  await client.assets.create([{name: 'new asset'}]);
+  await client.assets.create([{ name: 'new asset' }]);
   const rootAssets = await client.assets
     .list({ filter: { root: true } })
     .autoPagingToArray({ limit: 5 });
@@ -41,7 +39,7 @@ quickstart()
   .then(() => {
     process.exit(0);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
     process.exit(1);
   });
