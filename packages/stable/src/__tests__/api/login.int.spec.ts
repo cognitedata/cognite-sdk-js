ee3d952635580c87624a48b480d1c2cc1a73817c
birkmose <michael.birkmose@cognite.com>
2023-04-27 14:27:20 +0200

    feat(project): project update user profiles (#1009)
    * feat(project): project update user profiles

--- packages/stable/src/__tests__/api/login.int.spec.ts
+++ packages/stable/src/__tests__/api/login.int.spec.ts
@@ -1,7 +1,7 @@
 // Copyright 2020 Cognite AS
 
 import CogniteClient from '../../cogniteClient';
-import { setupClient, setupLoggedInClient } from '../testUtils';
+import { setupLoggedInClient } from '../testUtils';
 
 describe('Login-api integration test', () => {
   let client: CogniteClient;
@@ -15,19 +15,6 @@ describe('Login-api integration test', () => {
       const inspect = (await client.get('/api/v1/token/inspect')).data;
       expect(token).toBeDefined();
       expect(inspect.projects[0].projectUrlName).toBeDefined();
-    });
-
-    test('not logged in', async () => {
-      const anotherClient = setupClient();
-      await anotherClient.authenticate();
-      const status = await anotherClient.login.status();
-      expect(status).toBeNull();
-    });
-
-    test('invalid credentials', async () => {
-      const anotherClient = setupClient();
-      const status = await anotherClient.login.status();
-      expect(status).toBeNull();
     });
   });
 });
