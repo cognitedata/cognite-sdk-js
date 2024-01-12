cb54b84000e0356f2ed68f2702ce3725f0391de5
gulkaktus <david.wu@cognite.com>
2024-01-12 10:55:16 +0100

    chore: remove commented code

--- packages/stable/src/__tests__/api/instances.int.spec.ts
+++ packages/stable/src/__tests__/api/instances.int.spec.ts
@@ -4,56 +4,6 @@ import { ViewReference } from 'stable/src/api/instances/types.gen';
 import CogniteClient from '../../cogniteClient';
 import { setupLoggedInClient } from '../testUtils';
 
-// const getFileId = async (
-//   client: CogniteClient,
-//   deadline: number = 15000
-// ): Promise<number> => {
-//   // Use a previous uploaded file/document if available,
-//   // otherwise ingest a new file.
-
-//   async function lookup() {
-//     const resp = await client.documents.list({
-//       limit: 1,
-//     });
-//     return resp.items.length > 0 ? resp.items[0].id : undefined;
-//   }
-
-//   const found = await lookup();
-//   if (found) {
-//     return found;
-//   }
-
-//   const fileContent = new TextEncoder().encode('test data');
-//   await client.files.upload(
-//     {
-//       name: 'test.txt',
-//       mimeType: 'text/plain',
-//     },
-//     fileContent,
-//     false,
-//     true
-//   );
-
-//   // It takes some time for a uploaded file to become available
-//   // in the documents API.
-
-//   let timeLeft = deadline;
-//   while (timeLeft > 0) {
-//     const timeout = Math.min(timeLeft, 3000);
-//     await new Promise((resolve) => setTimeout(resolve, timeout));
-//     timeLeft -= timeout;
-
-//     const foundOnRetry = await lookup();
-//     if (foundOnRetry) {
-//       return foundOnRetry;
-//     }
-//   }
-
-//   throw new Error(
-//     'unable to ingest a file into the documents service within time'
-//   );
-// };
-
 describe('Instances integration test', () => {
   let client: CogniteClient;
   const describable: ViewReference = {
