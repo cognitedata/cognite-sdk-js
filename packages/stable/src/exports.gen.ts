f06771ba59b8d3b78b83d6f873b7488f18066186
gulkaktus <david.wu@cognite.com>
2024-01-15 14:30:36 +0100

    fix: lint exports.gen.ts

--- packages/stable/src/exports.gen.ts
+++ packages/stable/src/exports.gen.ts
@@ -1,7 +1,5 @@
 // Copyright 2022 Cognite AS
-export {
-  EpochTimestamp
-} from './types.gen';
+export { EpochTimestamp } from './types.gen';
 export {
   AnnotationData,
   AnnotationsAssetRef,
@@ -36,7 +34,7 @@ export {
   AnnotationsTextRegion,
   AnnotationsUnhandledSymbolObject,
   AnnotationsUnhandledTextObject,
-  AnnotationsView
+  AnnotationsView,
 } from './api/annotations/types.gen';
 export {
   CogniteExternalId,
@@ -115,8 +113,62 @@ export {
   DocumentsAggregateUniqueValuesResponse,
   DocumentsPreviewTemporaryLinkResponse,
   Label,
-  LabelList
+  LabelList,
 } from './api/documents/types.gen';
+export {
+  CDFExternalIdReference,
+  ContainerReference,
+  ContainsAllFilterV3,
+  ContainsAnyFilterV3,
+  CorePropertyDefinition,
+  DMSExistsFilter,
+  DMSExternalId,
+  DMSFilterProperty,
+  DMSVersion,
+  DataModelsBoolFilter,
+  DataModelsLeafFilter,
+  DataModelsNestedFilter,
+  DirectNodeRelation,
+  DirectRelationReference,
+  EdgeDefinition,
+  EqualsFilterV3,
+  FilterDefinition,
+  FilterValue,
+  FilterValueList,
+  FilterValueRange,
+  HasExistingDataFilterV3,
+  InFilterV3,
+  InstanceType,
+  LimitWithDefault1000,
+  MatchAllFilter,
+  NodeAndEdgeCollectionResponseV3Response,
+  NodeDefinition,
+  NodeOrEdge,
+  NodeOrEdgeExternalId,
+  NodeOrEdgeSearchRequest,
+  OverlapsFilterV3,
+  ParameterizedPropertyValueV3,
+  PrefixFilterV3,
+  PrimitiveProperty,
+  PropertyValueGroupV3,
+  RangeFilterV3,
+  RangeValue,
+  RawPropertyValueListV3,
+  RawPropertyValueV3,
+  ReferencedPropertyValueV3,
+  SearchRequestV3,
+  SourceReference,
+  SpaceSpecification,
+  TextProperty,
+  TypeInformation,
+  TypeInformationOuter,
+  TypePropertyDefinition,
+  TypingViewOrContainer,
+  ViewCorePropertyDefinition,
+  ViewDirectNodeRelation,
+  ViewOrContainer,
+  ViewReference,
+} from './api/instances/types.gen';
 export {
   CursorQueryParameter,
   IdentityType,
@@ -129,7 +181,7 @@ export {
   UserProfilesListResponse,
   UserProfilesNotFoundResponse,
   UserProfilesSearchRequest,
-  UserProfilesSearchResponse
+  UserProfilesSearchResponse,
 } from './api/userProfiles/types.gen';
 export {
   AssetTagDetection,
@@ -171,5 +223,5 @@ export {
   VisionExtractPostResponse,
   VisionExtractPredictions,
   VisionFileExternalId,
-  VisionFileId
+  VisionFileId,
 } from './api/vision/types.gen';
