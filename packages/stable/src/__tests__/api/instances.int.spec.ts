84e5ea23f265a54aeafc0aa83e4607a7d1262d7c
David Wu <75508558+gulkaktus@users.noreply.github.com>
2024-01-18 11:59:41 +0100

    feat: instances/search and instances/list (#1047)
    * chore: add types for instances/search
    
    * chore: add instances to client
    
    * chore: add tests
    
    * chore: remove commented code
    
    * fix: add missing newline
    
    * fix: update test
    
    * chore: simplify search endpoint
    
    * chore: upsert test data and space before tests, delete data after
    
    * fix: lint exports.gen.ts
    
    * chore: refactor test code
    
    * chore: add missing variable
    
    * fix: bug
    
    * chore: generate types for models/instances, removed nextCursor in QueryResponse for this to work
    
    * chore: add nextcursor manually, exclude instances endpoint from type generation to avoid overwriting
    
    * chore: add nextcursor back to openapi spec
    
    * chore: remove linting
    
    * chore: update resource url, add list endpoint
    
    * chore: linting
    
    * chore: add tests for list
    
    * chore: add code snippets
    
    * chore: move instance types from exports.gen.ts to separate types file
    
    * chore: fix snippets
    
    * chore: remove import statements from snippets
    
    * chore: fix bug in describable upsert in test
    
    * chore: fix snippet and upsert syntax error
    
    * chore: fix tests
    
    * chore: fix tests again

---
+++ packages/stable/src/__tests__/api/instances.int.spec.ts
@@ -0,0 +1,253 @@
+// Copyright 2024 Cognite AS
+
+import { ViewReference } from 'stable/src/types';
+import CogniteClient from '../../cogniteClient';
+import { setupLoggedInClient } from '../testUtils';
+
+type SpaceDefinition = {
+  space: string;
+  name: string;
+  description: string;
+};
+
+type Describable = {
+  externalId: string;
+  space: string;
+  title: string;
+  description: string;
+  labels: string[];
+};
+
+const upsertSpace = async (client: CogniteClient, space: SpaceDefinition) => {
+  await client.post(`/api/v1/projects/${client.project}/models/spaces`, {
+    data: {
+      items: [space],
+    },
+  });
+};
+
+const upsertDescribables = async (
+  client: CogniteClient,
+  describables: Describable[],
+  view: ViewReference
+) => {
+  await client.post(`/api/v1/projects/${client.project}/models/instances`, {
+    data: {
+      items: describables.map((describable) => ({
+        instanceType: 'node',
+        externalId: describable.externalId,
+        space: describable.space,
+        sources: [
+          {
+            source: view,
+            properties: {
+              title: describable.title,
+              description: describable.description,
+              labels: describable.labels,
+            },
+          },
+        ],
+      })),
+    },
+  });
+};
+
+describe('Instances integration test', () => {
+  let client: CogniteClient;
+  const timestamp = Date.now();
+
+  const testSpace: SpaceDefinition = {
+    space: `test_data_space`,
+    name: 'test_data_space',
+    description: 'Instance space used for integration tests.',
+  };
+  const view: ViewReference = {
+    externalId: 'Describable',
+    space: 'cdf_core',
+    type: 'view',
+    version: 'v1',
+  };
+
+  const describable1: Describable = {
+    externalId: `describable_1_${timestamp}`,
+    space: testSpace.space,
+    title: `title_1_${timestamp}`,
+    description: `description_1_${timestamp}`,
+    labels: [`label1`, 'label2'],
+  };
+  const describable2: Describable = {
+    externalId: `describable_2_${timestamp}`,
+    space: testSpace.space,
+    title: `title_2_${timestamp}`,
+    description: `description_2_${timestamp}`,
+    labels: [`label1`, 'label2'],
+  };
+
+  beforeAll(async () => {
+    client = setupLoggedInClient();
+    await upsertSpace(client, testSpace);
+    await upsertDescribables(client, [describable1, describable2], view);
+  });
+
+  afterAll(async () => {
+    await client.post(
+      `/api/v1/projects/${client.project}/models/instances/delete`,
+      {
+        data: {
+          items: [
+            {
+              instanceType: 'node',
+              externalId: describable1.externalId,
+              space: testSpace.space,
+            },
+            {
+              instanceType: 'node',
+              externalId: describable2.externalId,
+              space: testSpace.space,
+            },
+          ],
+        },
+      }
+    );
+  });
+
+  test('list nodes from a single view with limit 2', async () => {
+    const response = await client.instances.list({
+      sources: [{ source: view }],
+      instanceType: 'node',
+      limit: 2,
+    });
+    expect(response.items).toHaveLength(2);
+    expect(response.items[0].externalId).toBeDefined();
+    expect(response.items[1].externalId).toBeDefined();
+  });
+
+  test('list nodes with a filter that returns 1 result', async () => {
+    const response = await client.instances.list({
+      sources: [{ source: view }],
+      filter: {
+        equals: {
+          property: ['node', 'externalId'],
+          value: describable2.externalId,
+        },
+      },
+      instanceType: 'node',
+      limit: 1000,
+    });
+    expect(response.items).toHaveLength(1);
+    expect(response.items[0].externalId).toBeDefined();
+    expect(response.items[0].externalId).toBe(describable2.externalId);
+  });
+
+  test('list nodes with a filter that returns no results', async () => {
+    const response = await client.instances.list({
+      sources: [{ source: view }],
+      filter: {
+        equals: {
+          property: ['node', 'externalId'],
+          value: 'No describable has this title',
+        },
+      },
+      instanceType: 'node',
+      limit: 1000,
+    });
+    expect(response.items).toHaveLength(0);
+  });
+
+  test('list nodes sorted in ascending order', async () => {
+    const response = await client.instances.list({
+      sources: [{ source: view }],
+      sort: [
+        {
+          property: [view.space, `${view.externalId}/${view.version}`, 'title'],
+          direction: 'ascending',
+          nullsFirst: false,
+        },
+      ],
+      instanceType: 'node',
+      limit: 2,
+    });
+    expect(response.items).toHaveLength(2);
+    expect(response.items[0].externalId).toBeDefined();
+    expect(response.items[1].externalId).toBeDefined();
+
+    const title1 =
+      response.items[0].properties![view.space][
+        `${view.externalId}/${view.version}`
+      ]['title'].toString();
+    const title2 =
+      response.items[0].properties![view.space][
+        `${view.externalId}/${view.version}`
+      ]['title'].toString();
+    expect(title1 < title2);
+  });
+
+  test('list nodes sorted in descending order', async () => {
+    const response = await client.instances.list({
+      sources: [{ source: view }],
+      sort: [
+        {
+          property: [view.space, `${view.externalId}/${view.version}`, 'title'],
+          direction: 'descending',
+          nullsFirst: false,
+        },
+      ],
+      instanceType: 'node',
+      limit: 2,
+    });
+    expect(response.items).toHaveLength(2);
+    expect(response.items[0].externalId).toBeDefined();
+    expect(response.items[1].externalId).toBeDefined();
+
+    const title1 =
+      response.items[0].properties![view.space][
+        `${view.externalId}/${view.version}`
+      ]['title'].toString();
+    const title2 =
+      response.items[0].properties![view.space][
+        `${view.externalId}/${view.version}`
+      ]['title'].toString();
+    expect(title1 > title2);
+  });
+
+  test('search nodes with limit 2', async () => {
+    const response = await client.instances.search({
+      view,
+      instanceType: 'node',
+      limit: 2,
+    });
+    expect(response.items).toHaveLength(2);
+    expect(response.items[0].externalId).toBeDefined();
+    expect(response.items[1].externalId).toBeDefined();
+  });
+
+  test('search with query', async () => {
+    const response = await client.instances.search({
+      view,
+      query: describable1.description,
+      limit: 1,
+    });
+    expect(response.items).toHaveLength(1);
+    expect(response.items[0].externalId).toBe(describable1.externalId);
+  });
+
+  test('search with filter', async () => {
+    const response = await client.instances.search({
+      view,
+      filter: {
+        prefix: {
+          property: ['title'],
+          value: 'titl',
+        },
+      },
+      limit: 1,
+    });
+    expect(response.items).toHaveLength(1);
+    expect(response.items[0].properties);
+    const title =
+      response.items[0].properties![view.space][
+        `${view.externalId}/${view.version}`
+      ]['title'].toString();
+    expect(title.startsWith('titl'));
+  });
+});
---
+++ packages/stable/src/api/instances/instancesApi.ts
@@ -0,0 +1,96 @@
+// Copyright 2023 Cognite AS
+
+import { BaseResourceAPI, CDFHttpClient, MetadataMap } from '@cognite/sdk-core';
+import {
+  NodeAndEdgeCollectionResponseV3Response,
+  NodeAndEdgeCollectionResponseWithCursorV3Response,
+  NodeOrEdge,
+  NodeOrEdgeListRequestV3,
+  NodeOrEdgeSearchRequest,
+} from './types.gen';
+
+export class InstancesAPI extends BaseResourceAPI<NodeOrEdge> {
+  constructor(...args: [string, CDFHttpClient, MetadataMap]) {
+    super(...args);
+  }
+
+  /**
+   * [Search instances](https://developer.cognite.com/api#tag/Instances/operation/searchInstances)
+   *
+   * ```js
+   *  const response = await client.instances.search({
+   *    view: {
+   *      externalId: 'Describable',
+   *      space: 'cdf_core',
+   *      type: 'view',
+   *      version: 'v1',
+   *    },
+   *    query: 'your_query',
+   *    filter: {
+   *      equals: {
+   *        property: ['title'],
+   *         value: 'your title',
+   *      },
+   *    },
+   *    limit: 1000,
+   *  });
+   * ```
+   */
+  public search = async (
+    params: NodeOrEdgeSearchRequest
+  ): Promise<NodeAndEdgeCollectionResponseV3Response> => {
+    const response = await this.post<NodeAndEdgeCollectionResponseV3Response>(
+      this.searchUrl,
+      {
+        data: params,
+      }
+    );
+    return response.data;
+  };
+
+  /**
+   * [List instances](https://developer.cognite.com/api#tag/Instances/operation/advancedListInstance)
+   *
+   * ```js
+   *  const view = {
+   *    externalId: 'Describable',
+   *    space: 'cdf_core',
+   *    type: 'view',
+   *    version: 'v1',
+   *  };
+   *  const response = await client.instances.list({
+   *    instanceType: 'node',
+   *    sources: [{
+   *      source: {
+   *        externalId: 'Describable',
+   *        space: 'cdf_core',
+   *        type: 'view',
+   *        version: 'v1',
+   *      },
+   *    }],
+   *    filter: {
+   *      equals: {
+   *        property: ['title'],
+   *         value: 'your title',
+   *      },
+   *    },
+   *    sort: [
+   *      { property: ['title'], direction: 'ascending', nullsFirst: false },
+   *    ],
+   *    limit: 1000,
+   *});
+   * ```
+   */
+  public list = async (
+    params: NodeOrEdgeListRequestV3
+  ): Promise<NodeAndEdgeCollectionResponseWithCursorV3Response> => {
+    const response =
+      await this.post<NodeAndEdgeCollectionResponseWithCursorV3Response>(
+        this.listPostUrl,
+        {
+          data: params,
+        }
+      );
+    return response.data;
+  };
+}
