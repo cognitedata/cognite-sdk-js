a807ab6c5a9d9e28163077434ae25d7191245d10
Fredrik Anfinsen <fredrik.anfinsen@gmail.com>
2024-04-09 14:19:30 +0200

    feat: group membership (#1079)
    * feat: group membership
    
    * test: fix invalid request/response format

---
+++ packages/stable/src/__tests__/api/groups.unit.spec.ts
@@ -0,0 +1,58 @@
+// Copyright 2024 Cognite AS
+
+import nock from 'nock';
+import CogniteClient from '../../cogniteClient';
+import { setupMockableClient } from '../testUtils';
+import { mockBaseUrl } from '../testUtils';
+
+describe('Groups unit test', () => {
+  let client: CogniteClient;
+  beforeEach(() => {
+    client = setupMockableClient();
+    nock.cleanAll();
+  });
+
+  test('can create a CDF managed allUserAccounts group', async () => {
+    nock(mockBaseUrl)
+      .post(new RegExp('/groups'), {
+        items: [{ name: 'test-group', members: 'allUserAccounts' }],
+      })
+      .once()
+      .reply(200, {
+        items: [{ name: 'test-group', members: 'allUserAccounts' }],
+      });
+    await client.groups.create([
+      {
+        name: 'test-group',
+        members: 'allUserAccounts',
+      },
+    ]);
+  });
+
+  test('can create a CDF managed group', async () => {
+    nock(mockBaseUrl)
+      .post(new RegExp('/groups'), {
+        items: [
+          {
+            name: 'test-group',
+            members: ['a', 'b'],
+          },
+        ],
+      })
+      .once()
+      .reply(200, {
+        items: [
+          {
+            name: 'test-group',
+            members: ['a', 'b'],
+          },
+        ],
+      });
+    await client.groups.create([
+      {
+        name: 'test-group',
+        members: ['a', 'b'],
+      },
+    ]);
+  });
+});
