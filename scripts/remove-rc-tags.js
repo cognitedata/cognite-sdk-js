54e46c0272a2b236baafbdae1064fc8c9486df44
SudilHasithaCognite <sudil.abeyagunasekera@cognite.com>
2024-11-07 16:23:23 +0530

    fix(release): add custom script to remove rc tag

---
+++ scripts/remove-rc-tags.js
@@ -0,0 +1,21 @@
+// Remove -rc tag from release branch when it merge to master\
+// TODO: modify release.yaml to run this when merging from a release branch to the master branch
+const fs = require('fs');
+const glob = require('glob')
+
+// Retrive all pacakage.json file paths
+const packageFilesPaths = glob.sync('packages/*/package.json')
+
+// loop through each and remove rc tag
+packageFilesPaths.forEach(packageFilePath => {
+
+    //get the package content to json file
+    const packageJson = JSON.parse(fs.readFileSync(packageFilePath, 'utf-8'))
+
+    // check for rc, remove it, reassign the version and rewrite
+    if(packageJson.version && packageJson.version.includes('-rc')){
+        // 6.0.0-rc.1 --> [6.0.0, -rc.1]
+        packageJson.version = packageJson.version.split('-rc')[0];
+        fs.writeFileSync(packageFilePath, JSON.stringify(packageJson,null,2) + '\n');
+    }
+})
