dceb2c8ee9729d5c9bd74234106577a792523a50
Viet Le <viet.le@cognite.com>
2025-12-17 15:48:18 -0600

    export FunctionsAPI

--- samples/nodejs/oidc-typescript/quickstart.ts
+++ samples/nodejs/oidc-typescript/quickstart.ts
@@ -6,10 +6,11 @@ const project: string = process.env.COGNITE_PROJECT!;
 const clientId: string = process.env.CLIENT_ID!;
 const clientSecret: string = process.env.CLIENT_SECRET!;
 const azureTenant = process.env.AZURE_TENANT_ID!;
+const baseUrl = process.env.BASE_URL!;
 
-if (!project || !clientId || !clientSecret || !azureTenant) {
+if (!project || !clientId || !clientSecret || !azureTenant || !baseUrl) {
   throw Error(
-    'You must set the environment variable AZURE_TENANT_ID, COGNITE_PROJECT, CLIENT_ID and CLIENT_SECRET'
+    'You must set the environment variable AZURE_TENANT_ID, COGNITE_PROJECT, CLIENT_ID and CLIENT_SECRET and BASE_URL'
   );
 }
 
@@ -25,11 +26,11 @@ async function quickstart() {
   const client = new CogniteClient({
     appId: 'Cognite SDK samples',
     project,
-    baseUrl: 'https://api.cognitedata.com',
-    oidcTokenProvider: () =>
+    baseUrl,
+    getToken: () =>
       pca
         .acquireTokenByClientCredential({
-          scopes: ['https://api.cognitedata.com/.default'],
+          scopes: [`${baseUrl}/.default`],
           skipCache: true,
         })
         .then((response) => response?.accessToken! as string),
@@ -37,13 +38,17 @@ async function quickstart() {
 
   await client.authenticate();
 
-  const info = (await client.get('/api/v1/token/inspect')).data;
+  // const info = (await client.get('/api/v1/token/inspect')).data;
 
-  console.log('tokenInfo', JSON.stringify(info, null, 2));
+  // console.log('tokenInfo', JSON.stringify(info, null, 2));
 
   try {
-    const assets = await client.assets.list();
-    console.log(assets);
+    // const assets = await client.assets.list();
+    //const functions = await client.functions.list({ limit: 10 });
+    
+    // await client.functions.delete([functions[0].id]);
+    const retrieve = await client.functions.retrieve(1853152453344324);
+    console.log('retrieve', retrieve);
   } catch (e) {
     console.log('asset error');
     console.log(e);
