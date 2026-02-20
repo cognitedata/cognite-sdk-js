3d29c8892a0af47f4b57a2e6f2545c9f0de7eb3b
Pramod S <pramod.s@cognite.com>
2026-02-20 16:55:13 +0100

    revert lint setup changes

--- scripts/lint.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-// Wrapper so "yarn lint --fix" runs the fix script (biome ci does not accept --fix).
-const { execSync } = require('node:child_process');
-const hasFix = process.argv.includes('--fix');
-if (hasFix) {
-  execSync('yarn run lint:fix', { stdio: 'inherit' });
-} else {
-  execSync('yarn g:biome ci', { stdio: 'inherit' });
-}
